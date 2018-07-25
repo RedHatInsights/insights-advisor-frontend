6c1ca496fe795b6f3d5fa55056c1fd28a268c5fb
Brian McLaughlin <bmclaugh@redhat.com>
2018-07-25 13:13:52 -0400

    Base layout and styles for Actions page

--- src/SmartComponents/Actions/Actions.js
+++ src/SmartComponents/Actions/Actions.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import './actions.scss';
 
+import { Grid, GridItem } from '@patternfly/react-core';
 import { PageHeader } from '@red-hat-insights/insights-frontend-components';
 import { PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
+import { Section } from '@red-hat-insights/insights-frontend-components';
+import SummaryChart from '../../PresentationalComponents/SummaryChart/SummaryChart.js';
 
 /**
  * A smart component that handles all the api calls and data needed by the dumb components.
@@ -19,6 +23,18 @@ class Actions extends Component {
                 <PageHeader>
                     <PageHeaderTitle title='Actions'/>
                 </PageHeader>
+
+                <Section type='content'>
+                    <Grid gutter='md'>
+                        <GridItem span={4}>Donut</GridItem>
+                        <GridItem span={4}>
+                            <div className="pf-c-card ">
+                                <div className="pf-c-card__header ">Risk Summary</div>
+                                <div className="pf-c-card__body "><SummaryChart /></div>
+                            </div>
+                        </GridItem>
+                    </Grid>
+                </Section>
             </React.Fragment>
         );
     }
