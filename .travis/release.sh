2cd0a2b74c9189cf513dbb4f8a746a23943c39e8
Josh Kinlaw <jkinlaw@redhat.com>
2018-08-06 13:49:09 -0400

    fixing deployment process

--- .travis/release.sh
+++ .travis/release.sh
@@ -5,12 +5,13 @@ mkdir getJenkinsFile
 cd getJenkinsFile
 git clone https://github.com/RedHatInsights/insights-advisor-frontend-build.git
 cd insights-advisor-frontend-build
-cp Jenkinsfile ../../dist/Jenkinsfile
-cd ../../dist
+cp Jenkinsfile ../../Jenkinsfile
+cd ../..
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
-git add .
+git add Jenkinsfile
+git add dist
 git commit -m 'Build by Travis'
 git push --force --set-upstream travis-build master
--- .travis/release_stable.sh
+++ .travis/release_stable.sh
@@ -5,8 +5,8 @@ mkdir getJenkinsFile
 cd getJenkinsFile
 git clone https://github.com/RedHatInsights/insights-advisor-frontend-build.git
 cd insights-advisor-frontend-build
-cp Jenkinsfile ../../dist/Jenkinsfile
-cd ../../dist
+cp Jenkinsfile ../../Jenkinsfile
+cd ../..
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
@@ -14,6 +14,7 @@ git remote add travis-build ${REPO}.git
 git fetch origin
 git branch -v -a
 git checkout -b stable
-git add .
+git add Jenkinsfile
+git add dist
 git commit -m 'Build Stable by Travis'
 git push --force --set-upstream travis-build stable:stable
