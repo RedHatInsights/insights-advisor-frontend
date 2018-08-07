75a1cd016e36f433863062c3d01bbe391715db39
Josh Kinlaw <jkinlaw@redhat.com>
2018-08-07 07:03:01 -0400

    cleaning up build process

--- .travis/release_stable.sh
+++ .travis/release_stable.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-mkdir getJenkinsFile
-cd getJenkinsFile
-git clone https://github.com/RedHatInsights/insights-advisor-frontend-build.git
-cd insights-advisor-frontend-build
-cp Jenkinsfile ../../Jenkinsfile
-cd ../..
+git clone ${REPO}.git
+cd dist
+cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
-git fetch origin
-git branch -v -a
 git checkout -b stable
-git add Jenkinsfile
-git add dist
+git add .
 git commit -m 'Build Stable by Travis'
 git push --force --set-upstream travis-build stable:stable
