fe731dabd3c0ab88b73da811e88561676b92b7f6
Ryan Long <rlong@redhat.com>
2018-08-13 14:12:11 -0400

    adding tests

---
+++ src/PresentationalComponents/SummaryChart/SummaryChart.test.js
@@ -0,0 +1,22 @@
+import SummaryChart from './SummaryChart';
+import SummaryChartItem from '../SummaryChartItem/SummaryChartItem';
+
+import renderer from 'react-test-renderer';
+
+describe('SummaryChartItem', () => {
+    it('expects SummaryChart to render SummaryChartItem', () => {
+        const renders = renderer.create(
+            <SummaryChart>
+                <SummaryChartItem
+                    name={ 'withIssue' }
+                    numIssues={ 4 }
+                    totalIssues={ 4 }/>
+                <SummaryChartItem
+                    name={ 'noIssues' }
+                    numIssues={ 0 }
+                    totalIssues={ 4}/>
+            </SummaryChart>
+        ).toJSON();
+        expect(renders).toMatchSnapshot;
+    });
+});
