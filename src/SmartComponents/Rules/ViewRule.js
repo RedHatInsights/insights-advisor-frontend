6f964d7b43931079b4aeaf4ad5d45aa1fda008ef
Ryan Long <rlong@redhat.com>
2018-08-21 15:58:35 -0400

    skeleton example on view-rules

--- src/SmartComponents/Rules/ViewRule.js
+++ src/SmartComponents/Rules/ViewRule.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardHeader, CardBody, CardFooter } from '@patternfly/react-core';
+import { Section } from '@red-hat-insights/insights-frontend-components';
 
 import '../../PresentationalComponents/Skeletons/_Skeleton.scss';
 import '../../PresentationalComponents/RulesCard/_RulesCard.scss';
 
 const ViewRule = () => {
     return (
-        <Card className='ins-c-rules-card ins-c-card__skeleton'>
-            <CardHeader>
-                <div className='skeleton skeleton-md'>&nbsp;</div>
-            </CardHeader>
-            <CardBody>
-                <div className='skeleton skeleton-lg'>&nbsp;</div>
-            </CardBody>
-            <CardFooter>
-                <div className='skeleton skeleton-sm'>&nbsp;</div>
-            </CardFooter>
-        </Card>
+        <Section type='content'>
+            <Card className='ins-c-rules-card ins-c-card__skeleton'>
+                <CardHeader>
+                    <div className='skeleton skeleton-md'>&nbsp;</div>
+                </CardHeader>
+                <CardBody>
+                    <div className='skeleton skeleton-lg'>&nbsp;</div>
+                </CardBody>
+                <CardFooter>
+                    <div className='skeleton skeleton-sm'>&nbsp;</div>
+                </CardFooter>
+            </Card>
+            <Card className='ins-c-rules-card ins-c-card__skeleton'>
+                <CardHeader>
+                    <div className='skeleton skeleton-md'>&nbsp;</div>
+                </CardHeader>
+                <CardBody>
+                    <div className='skeleton skeleton-lg'>&nbsp;</div>
+                </CardBody>
+                <CardFooter>
+                    <div className='skeleton skeleton-sm'>&nbsp;</div>
+                </CardFooter>
+            </Card>
+            <Card className='ins-c-rules-card ins-c-card__skeleton'>
+                <CardHeader>
+                    <div className='skeleton skeleton-md'>&nbsp;</div>
+                </CardHeader>
+                <CardBody>
+                    <div className='skeleton skeleton-lg'>&nbsp;</div>
+                </CardBody>
+                <CardFooter>
+                    <div className='skeleton skeleton-sm'>&nbsp;</div>
+                </CardFooter>
+            </Card>
+            <Card className='ins-c-rules-card ins-c-card__skeleton'>
+                <CardHeader>
+                    <div className='skeleton skeleton-md'>&nbsp;</div>
+                </CardHeader>
+                <CardBody>
+                    <div className='skeleton skeleton-lg'>&nbsp;</div>
+                </CardBody>
+                <CardFooter>
+                    <div className='skeleton skeleton-sm'>&nbsp;</div>
+                </CardFooter>
+            </Card>
+        </Section>
     );
 };
 
