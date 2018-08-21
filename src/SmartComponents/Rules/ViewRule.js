b75fd8853bdf6c09866d903747315d8133660598
Ryan Long <rlong@redhat.com>
2018-08-21 13:55:16 -0400

    updating skeleton styling to better fit

--- src/SmartComponents/Rules/ViewRule.js
+++ src/SmartComponents/Rules/ViewRule.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardHeader, CardBody, CardFooter } from '@patternfly/react-core';
 
-const ViewRule = (props) => {
+import '../../PresentationalComponents/Skeletons/_Skeleton.scss';
+import '../../PresentationalComponents/RulesCard/_RulesCard.scss';
+
+const ViewRule = () => {
     return (
-        <h1>Rule {props.match.params.id}</h1>
+        <Card className='ins-c-rules-card ins-c-card__skeleton'>
+            <CardHeader>
+                <div className='skeleton skeleton-md'>&nbsp;</div>
+            </CardHeader>
+            <CardBody>
+                <div className='skeleton skeleton-lg'>&nbsp;</div>
+            </CardBody>
+            <CardFooter>
+                <div className='skeleton skeleton-sm'>&nbsp;</div>
+            </CardFooter>
+        </Card>
     );
 };
 
