0277136420edd2bd219584222fb2f61ce80b2fc1
Karel Hala <khala@redhat.com>
2018-10-05 16:59:35 +0200

    Use redux registry from frontend components

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,52 +1,16 @@
-import { applyMiddleware, combineReducers, createStore, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import { AdvisorStore } from '../AppReducer';
-
+import { compose } from 'redux';
+import { getRegistry } from '@red-hat-insights/insights-frontend-components';
 let registry;
 
-class ReducerRegistry {
-    constructor(initState = {}, middlewares = [], composeEnhancersDefault = compose) {
-        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancersDefault;
-        this.store = createStore(
-            (state = initState) => state,
-            initState,
-            composeEnhancers(applyMiddleware(...middlewares))
-        );
-        this.reducers = {};
-    }
-
-    on(event, callback) {
-        this.listenerMiddleware.addNew({ on: event, callback });
-    }
-
-    getListenerMiddleware() {
-        return this.listenerMiddleware;
-    }
-
-    getStore() {
-        return this.store;
-    }
-
-    /**
-     * Adds new reducers to the store
-     *
-     * @param newReducers the object of new reducers.
-     */
-    register(newReducers) {
-        this.reducers = { ...this.reducers, ...newReducers };
-        this.store.replaceReducer(combineReducers({ ...this.reducers }));
-    }
-}
-
 export function init (...middleware) {
-    if (registry) {
-        throw new Error('store already initialized');
-    }
-
-    registry = new ReducerRegistry({}, [
-        promiseMiddleware(),
-        ...middleware
-    ]);
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    registry = getRegistry(
+        {},
+        [ ...middleware, promiseMiddleware() ],
+        composeEnhancers
+    );
 
     registry.register({ AdvisorStore });
 
