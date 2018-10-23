14db0811e68032889f8a3a78dd1269126f27caa7
Allen Wight <allen.b.wight@gmail.com>
2018-10-23 15:57:21 -0400

    Reconfigures api to support production routes (#68)

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -6,7 +6,8 @@ export const IMPACTED_SYSTEMS_FETCH = 'IMPACTED_SYSTEMS_FETCH';
 export const STATS_FETCH = 'STATS_FETCH';
 export const BREADCRUMBS_SET = 'BREADCRUMBS_SET';
 
-export const RULES_FETCH_URL = '/rule/';
-export const STATS_FETCH_URL = '/stats/';
-export const SYSTEM_FETCH_URL = '/system/';
-export const SYSTEMTYPE_FETCH_URL = '/systemtype/';
+export const BASE_URL = '/r/insights/platform/advisor/v1';
+export const RULES_FETCH_URL = `${BASE_URL}/rule/`;
+export const STATS_FETCH_URL = `${BASE_URL}/stats/`;
+export const SYSTEM_FETCH_URL = `${BASE_URL}/system/`;
+export const SYSTEMTYPE_FETCH_URL = `${BASE_URL}/systemtype/`;
