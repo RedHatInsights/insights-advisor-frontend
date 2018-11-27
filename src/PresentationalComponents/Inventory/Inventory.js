4d814d425d554da4a985a313f817b4f7044beed6
Allen Wight <allen.b.wight@gmail.com>
2018-11-27 15:06:14 -0500

    Passes inventory items to inventory component (from IFC) (#99)

--- src/PresentationalComponents/Inventory/Inventory.js
+++ src/PresentationalComponents/Inventory/Inventory.js
@@ -4,6 +4,7 @@ import * as reactCore from '@patternfly/react-core';
 import * as reactIcons from '@patternfly/react-icons';
 import { registry } from '@red-hat-insights/insights-frontend-components';
 import Loading from '../../PresentationalComponents/Loading/Loading';
+import PropTypes from 'prop-types';
 
 @registry()
 class Inventory extends React.Component {
@@ -18,6 +19,7 @@ class Inventory extends React.Component {
     }
 
     async fetchInventory () {
+        const items = this.props.items;
         const { inventoryConnector, mergeWithEntities, mergeWithDetail } = await window.insights.loadInventory({
             react: React,
             reactRouterDom,
@@ -31,16 +33,22 @@ class Inventory extends React.Component {
         });
 
         this.setState({
-            Inventory: inventoryConnector()
+            Inventory: inventoryConnector(),
+            items
         });
     }
 
     render () {
-        const { Inventory } = this.state;
+        const { Inventory, items } = this.state;
+
         return (
-            <Inventory/>
+            <Inventory items={ items } />
         );
     }
 }
+
+Inventory.propTypes = {
+    items: PropTypes.array
+};
 
 export default Inventory;
