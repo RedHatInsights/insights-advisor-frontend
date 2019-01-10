2588647d2d60014892af3f6cb6da8f98a159cf7b
Allen Wight <awight@redhat.com>
2019-01-10 11:33:46 -0500

    Loading component now uses react-content-loader
    Done for continuity of insights frontend experience

--- src/PresentationalComponents/Loading/Loading.js
+++ src/PresentationalComponents/Loading/Loading.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Card, CardBody } from '@patternfly/react-core';
+import { List } from 'react-content-loader';
 
-const Loading = () => <div> Loading... </div>;
+const Loading = () => <>
+    <Card>
+        <CardBody>
+            <List />
+        </CardBody>
+    </Card></>;
 
 export default Loading;
