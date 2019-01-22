1d1751ee4e64603e94f61fb76a29f064e4fd47da
Brian McLaughlin <bmclaugh@redhat.com>
2019-01-22 14:45:09 -0500

    Refactor Breadcrumbs (#152)
    * Refactor breadcrumbs to make them match based and accept an options object

--- src/PresentationalComponents/Breadcrumbs/Breadcrumbs.js
+++ src/PresentationalComponents/Breadcrumbs/Breadcrumbs.js
@@ -4,36 +4,33 @@ import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
 import PropTypes from 'prop-types';
 import './_breadcrumbs.scss';
 
-export function buildBreadcrumbs(match, hops) {
-    let breadcrumbs = [];
-    breadcrumbs.push({
-        title: match.path.split('/')[1],
-        navigate: '/' + match.path.split('/')[1]
-    });
-    return parseBreadcrumbs(breadcrumbs, match.params, hops);
-}
+export function buildBreadcrumbs(match, options) {
+    let crumbs = [];
 
-export function parseBreadcrumbs(breadcrumbs, params, hops) {
-    if (breadcrumbs[0].navigate === '/rules') {
-        return breadcrumbs;
+    // add actions/rules base breadcrumb
+    if (options === undefined || options.breadcrummbs === undefined || options.breadcrumbs[0] === undefined) {
+        crumbs.push({ title: 'Actions', navigate: '/actions' });
     } else {
-        let crumbs = [];
-        if (hops >= 1) {
-            crumbs.push({
-                title: breadcrumbs[0].title,
-                navigate: breadcrumbs[0].navigate
-            });
-        }
-
-        if (hops === 2) {
-            crumbs.push({
-                title: params.type.replace('-', ' '),
-                navigate: breadcrumbs[0].navigate + '/' + params.type
-            });
-        }
-
-        return crumbs;
+        crumbs.push(options.breadcrumbs[0]);
     }
+
+    // add :type breadcrumb (exception: Rules based breadcrumbs)
+    if (match.params.id !== undefined && crumbs[0].title !== 'Rules') {
+        const title = match.params.type.indexOf('-') > -1 ? `${match.params.type.replace('-', ' ')} Actions` : match.params.type ;
+        crumbs.push({
+            title,
+            navigate: crumbs[0].navigate + '/' + match.params.type
+        });
+    }
+
+    if (match.params.inventoryId !== undefined) {
+        crumbs.push({
+            title: match.params.id,
+            navigate: crumbs[0].navigate + '/' + crumbs[1].navigate + '/' + match.params.id
+        });
+    }
+
+    return crumbs;
 }
 
 const Breadcrumbs = ({ items, current }) => (
@@ -43,12 +40,12 @@ const Breadcrumbs = ({ items, current }) => (
                 <Link to={ oneLink.navigate }>{ oneLink.title }</Link>
             </BreadcrumbItem>
         )) }
-        <BreadcrumbItem isActive>{ current }</BreadcrumbItem>
+        <BreadcrumbItem to='' isActive>{ current }</BreadcrumbItem>
     </Breadcrumb>
 );
 
 Breadcrumbs.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.any),
+    items: PropTypes.array,
     current: PropTypes.string
 };
 
