21712f2739838fe2ea6f4b3facbf09806f2bd586
Brian McLaughlin <bmclaugh@redhat.com>
2019-03-04 07:07:20 -0500

    address pr feedback

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,3 +1,4 @@
+/* eslint max-len: 0 */
 export const RULE_FETCH = 'RULE_FETCH';
 export const RULES_FETCH = 'RULES_FETCH';
 export const SYSTEM_FETCH = 'SYSTEM_FETCH';
@@ -59,18 +60,13 @@ export const FILTER_CATEGORIES = [
         ]
     }
 ];
-export const RISK_OF_CHANGE_LABEL = [ 'Very Low', 'Low', 'Moderate', 'High' ];
-export const RISK_OF_CHANGE_DESC = [
-    'The change takes very little time to implement and there is minimal impact to system operations. These are typically configuration changes that do not impact a running kernel or service. These changes can be made by modifying a line or file; the change that is taken is immediate and does not impact the system itself.',
-    'Typically, these changes do not require that a system be taken offline. They are safe to make and even if executed incorrectly, will still leave the systemin a usable state.',
-    'These will likely require an outage window. If these changes are executed incorrectly, they can leave the system in a severely degraded state.',
-    'The change takes a significant amount of time and planning to execute, and will impact the system and business operations of the host due to downtime.'
-];
-export const TOTAL_RISK_LABEL = [ 'Very Low', 'Low', 'Moderate', 'High' ];
-export const TOTAL_RISK_DESC = [
-    'Very low Total Risk description.',
-    'Low Total Risk description.',
-    'Moderate Total Risk description.',
-    'The likelihood that this will be a problem is low. The impact of the problem would be critical if it occurred.'
-];
-
+export const RISK_OF_CHANGE_DESC = {
+    1: 'The change takes very little time to implement and there is minimal impact to system operations. These are typically configuration changes that do not impact a running kernel or service. These changes can be made by modifying a line or file; the change that is taken is immediate and does not impact the system itself.',
+    2: 'Typically, these changes do not require that a system be taken offline. They are safe to make and even if executed incorrectly, will still leave the systemin a usable state.',
+    3: 'These will likely require an outage window. If these changes are executed incorrectly, they can leave the system in a severely degraded state.',
+    4: 'The change takes a significant amount of time and planning to execute, and will impact the system and business operations of the host due to downtime.'
+};
+export const IMPACT_LABEL = { 1: 'Low', 2: 'Medium', 3: 'High', 4: 'Critcal' };
+export const LIKELIHOOD_LABEL = { 1: 'Low', 2: 'Medium', 3: 'High', 4: 'Critcal' };
+export const RISK_OF_CHANGE_LABEL = { 1: 'Low', 2: 'Moderate', 3: 'Moderate', 4: 'High' };
+export const TOTAL_RISK_LABEL = { 1: 'Very Low', 2: 'Low', 3: 'Moderate', 4: 'High' };
--- src/PresentationalComponents/RuleDetails/RuleDetails.js
+++ src/PresentationalComponents/RuleDetails/RuleDetails.js
@@ -1,14 +1,18 @@
+/* eslint max-len: 0 */
 import React, { Component } from 'react';
-import { Ansible, Battery, Shield, routerParams } from '@red-hat-insights/insights-frontend-components';
+import { Battery, Shield, routerParams } from '@red-hat-insights/insights-frontend-components';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { Grid, GridItem } from '@patternfly/react-core';
 import { addNotification } from '@red-hat-insights/insights-frontend-components/components/Notifications';
 import ReactMarkdown from 'react-markdown/with-html';
+
 import * as AppActions from '../../AppActions';
-import { SYSTEM_TYPES, RISK_OF_CHANGE_LABEL, RISK_OF_CHANGE_DESC, TOTAL_RISK_LABEL, TOTAL_RISK_DESC } from '../../AppConstants';
+import * as AppConstants from '../../AppConstants';
 import Loading from '../../PresentationalComponents/Loading/Loading';
 import API from '../../Utilities/Api';
+import './_RuleDetails.scss';
 
 class RuleDetails extends Component {
     state = {
@@ -55,17 +59,25 @@ class RuleDetails extends Component {
         };
     }
 
+    ruleResolutionRisk = (rule) => {
+        return (rule.resolution_set.find(resolution => resolution.system_type === AppConstants.SYSTEM_TYPES.rhel).resolution_risk.risk);
+    };
+
+    ruleRebootRequired = (rule) => {
+        return rule.resolution_set.find(resolution => resolution.system_type === AppConstants.SYSTEM_TYPES.rhel).resolution_risk.reboot_required;
+    };
+
     render () {
-        const { children, ruleFetchStatus, rule } = this.props;
+        const { children, className, ruleFetchStatus, rule } = this.props;
         const { kbaDetails } = this.state;
         return (
             <>
                 { ruleFetchStatus === 'pending' && (<Loading/>) }
                 { ruleFetchStatus === 'fulfilled' && (
-                    <Grid gutter='md'>
+                    <Grid gutter='md' className={ className }>
                         <GridItem md={ 8 } sm={ 12 }>
                             <Grid>
-                                <GridItem className='actions__description'>
+                                <GridItem className='pf-u-pb-md'>
                                     {
                                         typeof rule.summary === 'string' &&
                                         Boolean(rule.summary) &&
@@ -73,40 +85,69 @@ class RuleDetails extends Component {
                                     }
                                 </GridItem>
                                 { kbaDetails.view_uri && (
-                                    <GridItem>
+                                    <GridItem className='pf-u-pb-md'>
                                         <a href={ kbaDetails.view_uri }>
                                             Knowledgebase Article
                                         </a>
                                     </GridItem>
                                 ) }
-                                <GridItem>Published: { `${(new Date(rule.publish_date)).toLocaleDateString()}` }</GridItem>
-                                <GridItem>Tags: { `${rule.tags || 'Not available'}` }</GridItem>
+                                { rule.tags && (
+                                    <GridItem>
+                                        Find Other Rules Related To:<br />
+                                        { rule.tags.replace(/ /g, ', ') }
+                                    </GridItem>
+                                ) }
                             </Grid>
                         </GridItem>
                         <GridItem md={ 4 } sm={ 12 }>
-                            <Grid gutter='sm' className='actions__detail'>
-                                <GridItem sm={ 8 } md={ 12 }>
-                                    <Grid className='ins-l-icon-group__vertical' sm={ 4 } md={ 12 }>
-                                        <div>Total Risk</div>
-                                        <GridItem><Battery label={ TOTAL_RISK_LABEL[rule.total_risk] } severity={ rule.total_risk }/></GridItem>
-                                        <div>{ TOTAL_RISK_DESC[rule.total_risk] }</div>
-                                    </Grid>
-                                </GridItem>
-                                <GridItem sm={ 4 } md={ 12 }>
-                                    <div>Risk of Change</div>
-                                    <Shield
-                                        hasTooltip={ false }
-                                        impact={ RISK_OF_CHANGE_LABEL[rule.resolution_set.find(resolution => resolution.system_type === SYSTEM_TYPES.rhel).resolution_risk.risk] }
-                                        size={ 'md' }
-                                        title={ RISK_OF_CHANGE_LABEL[rule.resolution_set.find(resolution => resolution.system_type === SYSTEM_TYPES.rhel).resolution_risk.risk] }
-                                    />
-                                    <div>{ RISK_OF_CHANGE_DESC[rule.resolution_set.find(resolution => resolution.system_type === SYSTEM_TYPES.rhel).resolution_risk.risk] }</div>
-                                </GridItem>
-                                { (
+                            <Grid gutter='sm'>
+                                { children && (
                                     <GridItem>
                                         { children }
                                     </GridItem>
                                 ) }
+                                <GridItem className='pf-u-pb-md' sm={ 8 } md={ 12 }>
+                                    <div>Total Risk</div>
+                                    <div className='pf-u-display-inline-flex pf-m-align-center pf-u-pb-sm pf-u-pt-sm'>
+                                        <div className='pf-u-display-inline-flex'>
+                                            <Battery
+                                                label=''
+                                                severity={ rule.total_risk }
+                                            />
+                                        </div>
+                                        <div className='pf-u-display-inline-flex'>
+                                            <span className={ `label pf-u-pl-sm ins-sev-clr-${rule.total_risk}` }>
+                                                { AppConstants.TOTAL_RISK_LABEL[rule.total_risk] || 'Undefined' }
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <p>
+                                        { `The likelihood that this will be a problem is ${AppConstants.LIKELIHOOD_LABEL[rule.likelihood] || 'Undefined'}. ` }
+                                        { `The impact of the problem would be ${AppConstants.IMPACT_LABEL[rule.impact.impact] || 'Undefined'} if it occurred.` }
+                                    </p>
+                                </GridItem>
+                                <GridItem sm={ 4 } md={ 12 } className='pf-l-flex'>
+                                    <div>Risk of Change</div>
+                                    <div className='pf-u-display-inline-flex pf-m-align-center pf-u-pb-sm pf-u-pt-sm'>
+                                        <div className='pf-u-display-inline-flex'>
+                                            <Shield
+                                                hasTooltip={ false }
+                                                impact={ this.ruleResolutionRisk(rule) }
+                                                size={ 'md' }
+                                                title={ AppConstants.RISK_OF_CHANGE_LABEL[this.ruleResolutionRisk(rule)] || 'Undefined' }
+                                            />
+                                        </div>
+                                        <div className='pf-u-display-inline-flex'>
+                                            <span className={ `label pf-u-pl-sm ins-sev-clr-${this.ruleResolutionRisk(rule)}` }>
+                                                { AppConstants.RISK_OF_CHANGE_LABEL[this.ruleResolutionRisk(rule)] || 'Undefined' }
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <p>{ AppConstants.RISK_OF_CHANGE_DESC[this.ruleResolutionRisk(rule)] }</p>
+                                </GridItem>
+                                { this.ruleRebootRequired && (<GridItem>
+                                    <span className='ins-sev-clr-4'>Restart Required</span>
+                                </GridItem>) }
                             </Grid>
                         </GridItem>
                     </Grid>
@@ -119,10 +160,10 @@ class RuleDetails extends Component {
 RuleDetails.propTypes = {
     addNotification: PropTypes.func.isRequired,
     children: PropTypes.any,
+    className: PropTypes.string,
     entities: PropTypes.any,
     fetchRule: PropTypes.func,
     fetchSystem: PropTypes.func,
-    match: PropTypes.any,
     rule: PropTypes.object,
     ruleFetchStatus: PropTypes.string
 };
@@ -134,10 +175,10 @@ const mapStateToProps = (state, ownProps) => ({
     ...ownProps
 });
 
-const mapDispatchToProps = dispatch => ({
-    fetchSystem: (url) => dispatch(AppActions.fetchSystem(url)),
-    addNotification: data => dispatch(addNotification(data))
-});
+const mapDispatchToProps = dispatch => bindActionCreators({
+    addNotification: data => addNotification(data),
+    fetchSystem: (url) => AppActions.fetchSystem(url)
+}, dispatch);
 
 export default routerParams(connect(
     mapStateToProps,
