08b047deb5b9d6c924f8ff3476937e567910a083
Allen Wight <awight@redhat.com>
2019-03-15 11:18:47 -0400

    Convert all paths to the cloud.redhat.com paths
    as per https://github.com/RedHatInsights/landing-page-frontend/commit/853ec2f75702baf70591c24ca1c0858b730b9723

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,9 +1,13 @@
 /* global exports */
-const openShiftAppsHost = `https://insights-advisor-api-advisor-ci.5a9f.insights-dev.openshiftapps.com/v1`;
+const host = `https://insights-advisor-api-advisor-ci.5a9f.insights-dev.openshiftapps.com/v1`;
+const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
 
 exports.routes = {
-    '/rule/': { host: openShiftAppsHost },
-    '/stats/': { host: openShiftAppsHost },
-    '/system/': { host: openShiftAppsHost },
-    '/systemtype/': { host: openShiftAppsHost }
+    '/rule/': { host },
+    '/stats/': { host },
+    '/system/': { host },
+    '/systemtype/': { host },
+    '/apps/chrome': { host: 'https://ci.cloud.paas.upshift.redhat.com' },
+    '/apps/insights/': { host: `http://${localhost}:8002` },
+    '/insights': { host: `http://${localhost}:8002` }
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { routerParams } from '@red-hat-insights/insights-frontend-components';
-import { matchPath } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { matchPath } from 'react-router-dom';
+
 import { Routes } from './Routes';
 import './App.scss';
 
@@ -10,13 +11,11 @@ class App extends Component {
     componentDidMount () {
         insights.chrome.init();
         insights.chrome.identifyApp('advisor');
-        insights.chrome.navigation(buildNavigation());
         this.appNav = insights.chrome.on('APP_NAVIGATION', event => {
-            if (!matchPath(location.href, { path: `${document.baseURI}platform/advisor/${event.navId}` })) {
+            if (!matchPath(location.href, { path: `${document.baseURI}insights/${event.navId}` })) {
                 this.props.history.push(`/${event.navId}`);
             }
         });
-        this.buildNav = this.props.history.listen(() => insights.chrome.navigation(buildNavigation()));
     }
 
     componentWillUnmount () {
@@ -39,17 +38,3 @@ export default routerParams(connect(
     null,
     null
 )(App));
-
-function buildNavigation () {
-    const currentPath = window.location.pathname.split('/').slice(-1)[0];
-    return [{
-        title: 'Actions',
-        id: 'actions'
-    }, {
-        title: 'Rules',
-        id: 'rules'
-    }].map(item => ({
-        ...item,
-        active: item.id === currentPath
-    }));
-}
