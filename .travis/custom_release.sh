81e741bf10358ae998b9f4b0ba074a901cf7bb59
Ian Page Hands <iphands@gmail.com>
2019-04-02 20:37:52 -0400

    fix custom deploy script. It was deleting the assets were just built

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -3,23 +3,32 @@ set -e
 set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    # for a limited time prod too
     for env in ci qa prod
     do
         echo
         echo
         echo "PUSHING ${env}-beta"
-        rm -rf ./dist
+
+        # note to Allen.. remove the temp .git dir
+        # not all of dist O_o
+        rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
     done
 fi
 
 if [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
+    # for a limited time prod too
     for env in ci qa prod
     do
         echo
         echo
         echo "PUSHING ${env}-beta"
-        rm -rf ./dist
+        rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
+fi
+
+if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    .travis/release.sh "${TRAVIS_BRANCH}"
 fi
