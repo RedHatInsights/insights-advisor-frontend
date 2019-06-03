e9f4631c50ae0ff07745196b786f7eca98b7c7e8
Allen Wight <awight@redhat.com>
2019-06-03 08:58:41 -0400

    Update overview donut to better align with pf styles (black tooltip)

--- src/PresentationalComponents/Charts/OverviewDonut.js
+++ src/PresentationalComponents/Charts/OverviewDonut.js
@@ -1,81 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { ChartDonut, ChartLabel, ChartLegend, ChartTheme } from '@patternfly/react-charts';
-import { Grid, GridItem } from '@patternfly/react-core';
+import { ChartDonut, ChartLegend, ChartThemeColor, ChartThemeVariant } from '@patternfly/react-charts';
 
 import './OverviewDonut.scss';
 
 class AdvisorOverviewDonut extends React.Component {
-    getChart = (theme, typeNames) => <ChartDonut
-        data={ this.props.category.map((value, key) => ({ x: typeNames[key], y: value, label: `${typeNames[key]}: ${value}` })) }
-        theme={ theme }
-        height={ 200 }
-        width={ 200 }
-        events={ [{
-            target: 'data',
-            eventHandlers: {
-                onClick: () => {
-                    return [
-                        {
-                            target: 'data',
-                            mutation: (props) => {
-                                this.props.history.push(`/overview/${props.datum.xName.toLowerCase()}`);
-                            }
-                        }
-                    ];
-                },
-                onMouseOver: () => {
-                    return [{
-                        mutation: (props) => {
-                            return {
-                                style: Object.assign({}, props.style, { fill: 'tomato', cursor: 'pointer' })
-                            };
-                        }
-                    }, {
-                        target: 'labels',
-                        mutation: () => ({ active: true })
-                    }];
-                },
-                onMouseOut: () => {
-                    return [{
-                        mutation: () => {
-                            return null;
-                        }
-                    }, {
-                        target: 'labels',
-                        mutation: () => ({ active: false })
-                    }];
-                }
-            }
-        }] }
-    />;
-
-    getLegend = (theme, typeNames) => <ChartLegend
-        data={ this.props.category.map((value, key) => ({ name: `${typeNames[key]} (${value})` })) }
-        itemsPerRow={ 1 }
-        events={ [
-            {
-                target: 'labels', eventHandlers: {
-                    onClick: this.legendClick,
-                    onMouseOver: () => {
-                        return [{
-                            mutation: (props) => {
-                                return {
-                                    style: Object.assign({}, props.style, { cursor: 'pointer' })
-                                };
-                            }
-                        }];
-                    }
-                }
-            }] }
-        theme={ theme }
-        gutter={ 0 }
-        width={ 60 }
-        height={ 60 }
-        y={ 30 }
-    />;
-
     legendClick = () => {
         return [{
             target: 'labels',
@@ -88,39 +18,60 @@ class AdvisorOverviewDonut extends React.Component {
     render () {
         const { className, category } = this.props;
         const totalHits = category.length ? category.reduce((sum, curr) => sum + curr) : 0;
-        const label = <svg className="chart-label" height={ 1 }>
-            <ChartLabel
-                style={ { fontSize: 20 } }
-                text={ totalHits }
-                textAnchor="middle"
-                verticalAnchor="middle"
-                x={ 88 }
-                y={ 66 }
-            />
-            <ChartLabel
-                style={ { fill: '#bbb' } }
-                text='Total hits'
-                textAnchor='middle'
-                verticalAnchor='middle'
-                x={ 88 }
-                y={ 89 }
-            />
-        </svg>;
         const typeNames = [ 'Availability', 'Stability', 'Performance', 'Security' ];
 
-        return <>{ totalHits ? <div className={ `chart-inline ${className}` }>
-            <Grid>
-                <GridItem span={ 6 }>
-                    <div className="chart-container">
-                        { label }
-                        { this.getChart(ChartTheme.light.multi, typeNames) }
-                    </div>
-                </GridItem>
-                <GridItem aria-label="Chart legend" span={ 6 }>
-                    { this.getLegend(ChartTheme.light.multi, typeNames) }
-                </GridItem>
-            </Grid>
-        </div>
+        return <>{ totalHits ?
+            <div className={ `donut-chart-inline ${className}` }>
+                <div className="donut-chart-container">
+                    <ChartDonut
+                        data={ this.props.category.map((value, key) => ({ x: typeNames[key], y: value, label: `${typeNames[key]}: ${value}` })) }
+                        labels={ datum => `${datum.x}: ${datum.y}` }
+                        events={ [{
+                            target: 'data',
+                            eventHandlers: {
+                                onClick: () => {
+                                    return [
+                                        {
+                                            target: 'data',
+                                            mutation: (props) => {
+                                                this.props.history.push(`/overview/${props.datum.xName.toLowerCase()}`);
+                                            }
+                                        }
+                                    ];
+                                }
+                            }
+                        }] }
+                        title={ `${totalHits}` }
+                        subTitle="Total Hits"
+                        themeColor={ ChartThemeColor.multi }
+                        themeVariant={ ChartThemeVariant.light }
+                    />
+                </div>
+                <ChartLegend
+                    data={ this.props.category.map((value, key) => ({ name: `${typeNames[key]} (${value})` })) }
+                    events={ [
+                        {
+                            target: 'labels', eventHandlers: {
+                                onClick: this.legendClick,
+                                onMouseOver: () => {
+                                    return [{
+                                        mutation: (props) => {
+                                            return {
+                                                style: Object.assign({}, props.style, { cursor: 'pointer' })
+                                            };
+                                        }
+                                    }];
+                                }
+                            }
+                        }] }
+                    orientation="vertical"
+                    height={ 200 }
+                    y={ 40 }
+                    responsive={ false }
+                    themeColor={ ChartThemeColor.multi }
+                    themeVariant={ ChartThemeVariant.light }
+                />;
+            </div>
             : <p style={ { marginTop: 18 } }>{ `Your connected systems have no categorized rule hits.` }</p>
         }</>;
     }
