b82204a6c748f7835582bb961db3b3cca4426a4d
Allen Wight <awight@redhat.com>
2019-06-25 15:49:11 -0400

    Refactor rulestable to functional component using hooks

---
+++ src/Utilities/Debounce.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+const Debounce = (value, delay) => {
+    const [ debouncedValue, setDebouncedValue ] = useState(value);
+    useEffect(() => {
+        const handler = setTimeout(() => {setDebouncedValue(value);}, delay);
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [ delay, value ]);
+
+    return debouncedValue;
+};
+
+export default Debounce;
