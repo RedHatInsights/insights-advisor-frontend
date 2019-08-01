9326885b394bde686d0f58e5c496e3d080feb515
Allen Wight <awight@redhat.com>
2019-08-01 12:05:17 -0400

    Tiny dep update, removal of console.log

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends Component {
                 overview() { insights.chrome.appNavClick({ id: 'overview' });},
                 rules() { insights.chrome.appNavClick({ id: 'rules' });}
             };
-            console.error(appNavClick[baseComponentUrl]);
             appNavClick[baseComponentUrl]();
         }
     }
