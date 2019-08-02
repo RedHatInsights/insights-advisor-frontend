2c2fdfdd9b3a44269e36170367bf7633f4cdaf69
Dan Kuc <dkuc@redhat.com>
2019-08-02 15:52:08 -0400

    Cleanup spandx config to make it easier to connect to a local api

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,13 +1,11 @@
 /* global exports */
-const host = `https://ci.cloud.paas.psi.redhat.com`;
-const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
+const apiHost = `https://ci.cloud.paas.psi.redhat.com`;
+const frontendHost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
+const chromeHost = 'https://ci.cloud.paas.psi.redhat.com';
 
 exports.routes = {
-    '/rule/': { host },
-    '/stats/': { host },
-    '/system/': { host },
-    '/systemtype/': { host },
-    '/apps/chrome': { host },
-    '/apps/insights/': { host: `http://${localhost}:8002` },
-    '/insights': { host: `http://${localhost}:8002` }
+    '/api/insights/v1/': { host: apiHost },
+    '/apps/chrome': { host: chromeHost },
+    '/apps/insights/': { host: `http://${frontendHost}:8002` },
+    '/insights': { host: `http://${frontendHost}:8002` }
 };
