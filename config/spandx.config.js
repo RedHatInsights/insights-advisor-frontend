bfdb419169d22dbe746520640dd33f6d575e5960
Allen Wight <awight@redhat.com>
2019-09-06 09:06:04 -0400

    Update cnames (#407)

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,7 +1,7 @@
 /* global exports */
-const apiHost = `https://ci.cloud.paas.psi.redhat.com`;
+const apiHost = `https://cloud.redhat.com`;
 const frontendHost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
-const chromeHost = 'https://ci.cloud.paas.psi.redhat.com';
+const chromeHost = 'https://ci.cloud.redhat.com';
 
 exports.routes = {
     '/api/insights/v1/': { host: apiHost },
