9a7c420714301b3173b3182d924bfddf6f16cd13
Allen Wight <awight@redhat.com>
2019-10-15 16:16:24 -0400

    Address ux feedback for rules details component (#453)
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2750

--- src/PresentationalComponents/RuleDetails/RuleDetails.js
+++ src/PresentationalComponents/RuleDetails/RuleDetails.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Battery, Reboot, Shield } from '@redhat-cloud-services/frontend-components';
 import PropTypes from 'prop-types';
-import { Grid, GridItem, Card, CardBody } from '@patternfly/react-core';
+import { Grid, GridItem, Card, CardBody, Text, TextContent, TextVariants } from '@patternfly/react-core';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import ReactMarkdown from 'react-markdown/with-html';
 import { injectIntl } from 'react-intl';
@@ -26,11 +26,11 @@ const RuleDetails = ({ children, className, rule, intl, topics }) => {
 
     const topicLinks = () => topics && compact(topics.map((topic) =>
         intersection(topic.tags.split(' '), rule.tags.split(' ')).length &&
-            <React.Fragment key={topic.slug}>
-                <Link to={`/topics/${topic.slug}`}>
-                    {`${topic.name}`}
-                </Link>
-            </React.Fragment>
+        <React.Fragment key={topic.slug}>
+            <Link to={`/topics/${topic.slug}`}>
+                {`${topic.name}`}
+            </Link>
+        </React.Fragment>
     ));
 
     return <Grid gutter='md' className={className}>
@@ -72,7 +72,7 @@ const RuleDetails = ({ children, className, rule, intl, topics }) => {
                 )}
                 <GridItem className='pf-u-pb-md' sm={8} md={12}>
                     <div>{intl.formatMessage(messages.totalRisk)}</div>
-                    <div className='pf-u-display-inline-flex pf-m-align-center pf-u-pb-sm pf-u-pt-sm'>
+                    <div className='pf-u-display-inline-flex alignCenterOverride pf-u-pb-sm pf-u-pt-sm'>
                         <div className='pf-u-display-inline-flex'>
                             <Battery
                                 label=''
@@ -80,20 +80,22 @@ const RuleDetails = ({ children, className, rule, intl, topics }) => {
                             />
                         </div>
                         <div className='pf-u-display-inline-flex'>
-                            <span className={`label pf-u-pl-sm ins-sev-clr-${rule.total_risk}`}>
+                            <span className={`batteryTextMarginOverride pf-u-pl-sm ins-sev-clr-${rule.total_risk}`}>
                                 {AppConstants.TOTAL_RISK_LABEL[rule.total_risk] || intl.formatMessage(messages.undefined)}
                             </span>
                         </div>
                     </div>
-                    <p>{intl.formatMessage(messages.rulesDetailsTotalriskBody, {
-                        likelihood: AppConstants.LIKELIHOOD_LABEL[rule.likelihood] || intl.formatMessage(messages.undefined),
-                        impact: (rule.impact && AppConstants.IMPACT_LABEL[rule.impact.impact]) || intl.formatMessage(messages.undefined),
-                        strong(str) { return <strong key={str}>{str}</strong>; }
-                    })}</p>
+                    <TextContent>
+                        <Text component={TextVariants.small}>{intl.formatMessage(messages.rulesDetailsTotalriskBody, {
+                            likelihood: AppConstants.LIKELIHOOD_LABEL[rule.likelihood] || intl.formatMessage(messages.undefined),
+                            impact: (rule.impact && AppConstants.IMPACT_LABEL[rule.impact.impact]) || intl.formatMessage(messages.undefined),
+                            strong(str) { return <strong key={str}>{str}</strong>; }
+                        })}</Text>
+                    </TextContent>
                 </GridItem>
                 <GridItem sm={4} md={12}>
                     <div>{intl.formatMessage(messages.riskofchange)}</div>
-                    <div className='pf-u-display-inline-flex pf-m-align-center pf-u-pb-sm pf-u-pt-sm'>
+                    <div className='pf-u-display-inline-flex alignCenterOverride pf-u-pb-sm pf-u-pt-sm'>
                         <div className='pf-u-display-inline-flex'>
                             <Shield
                                 hasTooltip={false}
@@ -108,7 +110,11 @@ const RuleDetails = ({ children, className, rule, intl, topics }) => {
                             </span>
                         </div>
                     </div>
-                    <p>{AppConstants.RISK_OF_CHANGE_DESC[resolutionRisk]}</p>
+                    <TextContent>
+                        <Text component={TextVariants.small}>
+                            {AppConstants.RISK_OF_CHANGE_DESC[resolutionRisk]}
+                        </Text>
+                    </TextContent>
                 </GridItem>
                 {rule.reboot_required && <Reboot red />}
             </Grid>
