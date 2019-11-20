20f133c21fb8644d539f5140686835c74263320f
Allen Wight <awight@redhat.com>
2019-11-20 08:01:26 -0500

    Refactors app.js to functional component
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2926

--- src/App.js
+++ src/App.js
@@ -1,59 +1,46 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import routerParams from '@redhat-cloud-services/frontend-components-utilities/files/RouterParams';
-import { connect } from 'react-redux';
-import { matchPath } from 'react-router-dom';
-
-import { Routes } from './Routes';
 import './App.scss';
 
-class App extends Component {
-    componentDidMount() {
-        insights.chrome.init();
-        insights.chrome.identifyApp('insights');
-        this.appNav = insights.chrome.on('APP_NAVIGATION', event => {
-            if (event.domEvent) {
-                this.props.history.push(`/${event.navId}`);
-            }
+import React, { useEffect, useState } from 'react';
 
-            if (location.pathname.indexOf(event.navId) === -1 &&
-                !matchPath(location.href, { path: `${document.baseURI}insights/${event.navId}` })
-            ) {
-                this.props.history.push(`/${event.navId}`);
+import PropTypes from 'prop-types';
+import { Routes } from './Routes';
+import routerParams from '@redhat-cloud-services/frontend-components-utilities/files/RouterParams';
+
+const App = (props) => {
+    const [auth, setAuth] = useState(false);
+    const appNavClick = {
+        overview(redirect) { insights.chrome.appNavClick({ id: 'overview', redirect  }); },
+        rules(redirect) { insights.chrome.appNavClick({ id: 'rules', redirect  }); },
+        topics(redirect) { insights.chrome.appNavClick({ id: 'topics', redirect }); }
+    };
+
+    useEffect(() => {
+        insights.chrome.init();
+        insights.chrome.auth.getUser().then(() => setAuth(true));
+        insights.chrome.identifyApp('insights');
+        const baseComponentUrl = props.location.pathname.split('/')[1];
+        const unregister = insights.chrome.on('APP_NAVIGATION', event => {
+            if (event.domEvent) {
+                props.history.push(`/${event.navId}`);
+                appNavClick[baseComponentUrl](true);
             }
         });
-    }
 
-    componentDidUpdate(prevProps) {
-        if (this.props !== prevProps) {
-            const baseComponentUrl = location.pathname.slice(location.pathname.indexOf('insights/')).split('/')[1];
-            const appNavClick = {
-                topics() { insights.chrome.appNavClick({ id: 'topics' }); },
-                rules() { insights.chrome.appNavClick({ id: 'rules' }); }
-            };
-            if (appNavClick[baseComponentUrl]) {
-                appNavClick[baseComponentUrl]();
-            }
-        }
-    }
+        return () => unregister();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    componentWillUnmount() {
-        this.appNav();
-    }
+    useEffect(() => {
+        const baseComponentUrl = props.location.pathname.split('/')[1];
+        appNavClick[baseComponentUrl](false);
+    }, [appNavClick, props.location]);
 
-    render() {
-        return (
-            <Routes childProps={this.props} />
-        );
-    }
-}
+    return (auth && <Routes childProps={props} />);
+};
 
 App.propTypes = {
     location: PropTypes.object,
     history: PropTypes.object
 };
 
-export default routerParams(connect(
-    null,
-    null
-)(App));
+export default routerParams(App);
