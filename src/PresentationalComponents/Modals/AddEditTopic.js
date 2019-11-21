1828ca961f07ea5900cd99c28b257bed656d256d
LI0131 <mccannliam.lm@gmail.com>
2019-11-21 14:53:26 -0500

    Add Topics Admin table and Modal

---
+++ src/PresentationalComponents/Modals/AddEditTopic.js
@@ -0,0 +1,218 @@
+import './_AddEditTopic.scss';
+
+import {
+    Button,
+    Checkbox,
+    Form,
+    FormGroup,
+    Modal,
+    Radio,
+    Split,
+    SplitItem,
+    TextArea,
+    TextInput
+} from '@patternfly/react-core';
+import React, { useState } from 'react';
+
+import API from '../../Utilities/Api';
+import { BASE_URL } from '../../AppConstants';
+import PropTypes from 'prop-types';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
+import { connect } from 'react-redux';
+import { fetchTopicsAdmin } from '../../AppActions';
+import { injectIntl } from 'react-intl';
+import messages from '../../Messages';
+
+const AddEditTopic = ({ handleModalToggle, intl, isModalOpen, topic, addNotification, fetchTopicsAdmin }) => {
+
+    const [name, setName] = useState(topic.name || '');
+    const [description, setDescription] = useState(topic.description || '');
+    const [tag, setTag] = useState(topic.tag || '');
+    const [enabled, setEnabled] = useState(topic.enabled || false);
+    const [featured, setFeatured] = useState(topic.featured || false);
+    const [slug, setSlug] = useState(topic.slug || undefined);
+
+    const editTopic = async ({ type }) => {
+        try {
+            const data = { name, slug, tag, description, enabled, featured };
+            if (type === 'DELETE') {
+                await API.delete(`${BASE_URL}/topic/${slug}`);
+            } else if (topic.slug) {
+                await API.put(`${BASE_URL}/topic/${slug}`, data, {});
+            } else {
+                await API.post(`${BASE_URL}/topic/`, {}, data);
+            }
+        } catch (error) {
+            addNotification({
+                variant: 'danger',
+                dismissable: true,
+                title: intl.formatMessage(messages.error),
+                description: Object.entries(error.response.data).map(([key, value]) => `${key.toUpperCase()}:${value} `)
+            });
+        } finally {
+            handleModalToggle(false);
+            fetchTopicsAdmin();
+        }
+    };
+
+    const setNameAndSlug = (name) => {
+        if (topic.slug) {
+            setName(name);
+        } else {
+            setName(name);
+            setSlug(name.replace(/\s/g, '').toLowerCase());
+        }
+    };
+
+    const footer = (
+        <Split gutter='sm' className='split-override'>
+            <SplitItem>
+                <Button key='confirm' variant='primary' onClick={ () => editTopic({ type: 'POST/PUT' }) }>
+                    {intl.formatMessage(messages.save)}
+                </Button>
+            </SplitItem>
+            <SplitItem>
+                <Button key='cancel' variant='secondary' onClick={ () => handleModalToggle(false) }>
+                    {intl.formatMessage(messages.cancel)}
+                </Button>
+            </SplitItem>
+            <SplitItem isFilled/>
+            <SplitItem>
+                <Button
+                    key='delete'
+                    variant='danger'
+                    isDisabled={ topic.slug ? false : true }
+                    onClick={ () => editTopic({ type: 'DELETE' }) }
+                >
+                    {intl.formatMessage(messages.deleteTopic)}
+                </Button>
+            </SplitItem>
+        </Split>
+    );
+
+    return <Modal
+        title={intl.formatMessage(messages.topicAdminTitle)}
+        isOpen={isModalOpen}
+        onClose={ () => handleModalToggle(false) }
+        footer={footer}
+        isFooterLeftAligned
+        className='modal-width-override'
+    >
+        <Form>
+            <FormGroup
+                label={intl.formatMessage(messages.name)}
+                fieldId='topic-form-name'
+                className='text-input-override'
+            >
+                <TextInput
+                    value={name}
+                    isRequired
+                    type='text'
+                    id='topic-form-name'
+                    name='topic-form-name'
+                    onChange={ (name) => setNameAndSlug(name) }
+                />
+            </FormGroup>
+            <FormGroup
+                label={intl.formatMessage(messages.topicAddEditDescription)}
+                fieldId='topic-form-description'
+                helperText={intl.formatMessage(
+                    messages.topicAddEditDescriptionHelperText
+                )}
+                className='text-area-override'
+            >
+                <TextArea
+                    value={description}
+                    isRequired
+                    name='topic-form-description'
+                    id='topic-form-description'
+                    onChange={ (desc) => setDescription(desc) }
+                />
+            </FormGroup>
+            <FormGroup isInline fieldId='topic-form-labels'>
+                <FormGroup
+                    label={intl.formatMessage(messages.tag)}
+                    fieldId='topic-form-tag'
+                    helperText={intl.formatMessage(
+                        messages.topicAddEditTagHelperText
+                    )}
+                    className='text-input-override'
+                >
+                    <TextInput
+                        value={tag.replace(/\s/g, '').toLowerCase()}
+                        isRequired
+                        type='text'
+                        id='topic-form-tag'
+                        name='topic-form-tag'
+                        onChange={ (tag) => setTag(tag.replace(/\s/g, '').toLowerCase()) }
+                    />
+                </FormGroup>
+                <FormGroup
+                    label={intl.formatMessage(messages.topicSlug)}
+                    fieldId='topic-form-name-2'
+                    helperText=''
+                    className='text-input-override'
+                >
+                    <TextInput
+                        value={ slug || name.replace(/\s/g, '').toLowerCase()}
+                        isDisabled={ topic.slug }
+                        type='text'
+                        id='topic-form-name-2'
+                        name='topic-form-name-2'
+                        onChange={ (name) => setNameAndSlug(name) }
+                    />
+                </FormGroup>
+            </FormGroup>
+            <FormGroup
+                label={intl.formatMessage(messages.status)}
+                fieldId='topic-form-enabled'
+            >
+                <Radio
+                    isChecked={ !enabled }
+                    id='disabled'
+                    label={intl.formatMessage(messages.topicAddEditDisabled)}
+                    onChange={ () => setEnabled(!enabled) }
+                    className='radio-override'
+                />
+                <Radio
+                    isChecked={ enabled }
+                    id='enabled'
+                    label={intl.formatMessage(messages.topicAddEditEnabled)}
+                    onChange={ () => setEnabled(!enabled) }
+                    className='radio-override'
+                />
+            </FormGroup>
+            <FormGroup
+                label={intl.formatMessage(messages.featured)}
+                fieldId='topic-form-featured'
+            >
+                <Checkbox
+                    isChecked={ featured }
+                    label={intl.formatMessage(messages.topicAddEditFeatureBox)}
+                    id='checkbox-featured'
+                    name='checkbox-featured'
+                    aria-label='update-featured'
+                    onChange={ () => setFeatured(!featured) }
+                    className='checkbox-override'
+                />
+            </FormGroup>
+        </Form>
+    </Modal>;
+
+};
+
+AddEditTopic.propTypes = {
+    handleModalToggle: PropTypes.func,
+    isModalOpen: PropTypes.bool,
+    topic: PropTypes.object,
+    intl: PropTypes.any,
+    addNotification: PropTypes.func,
+    fetchTopicsAdmin: PropTypes.func
+};
+
+const mapDispatchToProps = dispatch => ({
+    addNotification: data => dispatch(addNotification(data)),
+    fetchTopicsAdmin: () => dispatch(fetchTopicsAdmin())
+});
+
+export default injectIntl(connect(null, mapDispatchToProps)(AddEditTopic));
--- src/PresentationalComponents/TopicsAdminTable/TopicsAdminTable.js
+++ src/PresentationalComponents/TopicsAdminTable/TopicsAdminTable.js
@@ -1,8 +1,199 @@
-import React from 'react';
+import './_TopicsAdminTable.scss';
 
-const TopicsAdminTable = () => {
+import * as AppActions from '../../AppActions';
 
-    return <React.Fragment>hi</React.Fragment>;
+import { BanIcon, CheckCircleIcon, EditAltIcon, StarIcon } from '@patternfly/react-icons';
+import { Button, Title } from '@patternfly/react-core';
+import { Main, PageHeader, PrimaryToolbar, TableToolbar } from '@redhat-cloud-services/frontend-components';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Table, TableBody, TableHeader, sortable } from '@patternfly/react-table';
+
+import AddEditTopic from '../Modals/AddEditTopic';
+import Failed from '../Loading/Failed';
+import Immutable from 'seamless-immutable';
+import Loading from '../Loading/Loading';
+import MessageState from '../MessageState/MessageState';
+import PropTypes from 'prop-types';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
+import messages from '../../Messages';
+import routerParams from '@redhat-cloud-services/frontend-components-utilities/files/RouterParams';
+
+const TopicsAdminTable = ({ topicsFetchStatus, fetchTopicsAdmin, topics, intl }) => {
+
+    const [cols] =  useState([
+        { title: intl.formatMessage(messages.title), transforms: [sortable] },
+        { title: intl.formatMessage(messages.tag), transforms: [sortable] },
+        { title: intl.formatMessage(messages.topicSlug), transforms: [sortable] },
+        { title: intl.formatMessage(messages.status), transforms: [sortable] },
+        { title: intl.formatMessage(messages.featured), transforms: [sortable] },
+        '', ''
+    ]);
+    const [rows, setRows] = useState([]);
+    const [sortBy, setSortBy] = useState({});
+    const [addEditTopicOpen, setAddEditTopicOpen] = useState(false);
+    const [selectedTopic, setSelectedTopic] = useState({});
+    const [topicsArray, setTopicsArray] = useState([]);
+
+    const onSort = useCallback((_event, index, direction) => {
+        const attrIndex = {
+            0: 'name',
+            1: 'tag',
+            2: 'slug',
+            3: 'enabled',
+            4: 'featured'
+        };
+        const attr = attrIndex[index];
+        setSortBy({ index, direction });
+        setTopicsArray(
+            Immutable.from(
+                topicsArray.asMutable().sort((a, b) => {
+                    if (direction === 'asc') {
+                        if (attr === 'enabled' || attr === 'featured') {
+                            return a[attr] > b[attr] ? -1 : 1;
+                        } else { return a[attr] > b[attr] ? 1 : -1; }
+                    } else {
+                        if (attr === 'enabled' || attr === 'featured') {
+                            return a[attr] > b[attr] ? 1 : -1;
+                        } else { return a[attr] > b[attr] ? -1 : 1; }
+                    }
+                })
+            )
+        );
+    }, [setSortBy, setTopicsArray, topicsArray]);
+
+    const hideTopics = (topic) => {
+        if (!topic) {
+            setAddEditTopicOpen(true);
+        } else {
+            setSelectedTopic(topic);
+            setAddEditTopicOpen(true);
+        }
+    };
+
+    useEffect(() => {
+        setTopicsArray(topics);
+    }, [topics]);
+
+    useEffect(() => {
+        fetchTopicsAdmin();
+    }, [fetchTopicsAdmin]);
+
+    useEffect(() => {
+        if (topicsArray.length === 0) {
+            setRows([{
+                cells: [{
+                    title: (
+                        <MessageState icon='none' title={''}
+                            text={''}>
+                        </MessageState>),
+                    props: { colSpan: 7 }
+                }]
+            }]);
+        } else {
+            const rows = topicsArray.flatMap((value, key) => ([{
+                isOpen: false,
+                rule: value,
+                cells: [{
+                    title: <span key={key}> {value.name}</span>
+                }, {
+                    title: <span key={key}> {value.tag}</span>
+                }, {
+                    title: <span key={key}> {value.slug}</span>
+                }, {
+                    title: <span>
+                        {value.enabled ? <span>
+                            <CheckCircleIcon
+                                className='success'
+                            /> {intl.formatMessage(messages.enabled)}
+                        </span> : <span>
+                            <BanIcon
+                                className='ban'
+                            /> {intl.formatMessage(messages.disabled)}
+                        </span>}
+                    </span>
+                }, {
+                    title: <span>
+                        {value.featured ? <span>
+                            <StarIcon/> {intl.formatMessage(messages.featured)}
+                        </span> : <span></span>
+                        }
+                    </span>
+                }, '', {
+                    title: <Button
+                        variant='link'
+                        onClick={ () => hideTopics(value) }
+                    ><EditAltIcon/> {intl.formatMessage(messages.topicAdminEdit)}</Button>
+                }]
+            }]));
+            setRows(rows.asMutable());
+        }
+    }, [topicsArray, intl]);
+
+    return <React.Fragment>
+        {addEditTopicOpen && <AddEditTopic
+            isModalOpen={addEditTopicOpen}
+            handleModalToggle={setAddEditTopicOpen}
+            topic={selectedTopic}
+        />}
+        <PageHeader>
+            <Title headingLevel="h3" size="2xl" className='titlePaddingOverride'>
+                {intl.formatMessage(messages.topicAdminTitle)}
+            </Title>
+        </PageHeader>
+        <Main>
+            <React.Fragment>
+                <PrimaryToolbar className='toolbar-padding-override'>
+                    <Button
+                        variant='primary'
+                        onClick={ (_) => hideTopics(_) }
+                    >
+                        {intl.formatMessage(messages.topicAdminCreate)}
+                    </Button>
+                </PrimaryToolbar>
+                { topicsFetchStatus === 'fulfilled' &&
+                    <Table
+                        aria-label={'topics-admin-table'}
+                        sortBy={sortBy}
+                        onSort={onSort}
+                        cells={cols}
+                        rows={rows}
+                    >
+                        <TableHeader/>
+                        <TableBody/>
+                    </Table>
+                }
+                { topicsFetchStatus === 'pending' && (<Loading />)}
+                { topicsFetchStatus === 'failed' && (<Failed message={intl.formatMessage(messages.systemTableFetchError)} />)}
+                <TableToolbar/>
+            </React.Fragment>
+        </Main>
+    </React.Fragment>;
+
 };
 
-export default TopicsAdminTable;
+TopicsAdminTable.propTypes = {
+    fetchTopicsAdmin: PropTypes.func,
+    topicsFetchStatus: PropTypes.string,
+    topics: PropTypes.array,
+    addNotification: PropTypes.func,
+    history: PropTypes.object,
+    intl: PropTypes.any
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    topics: state.AdvisorStore.topics,
+    topicsFetchStatus: state.AdvisorStore.topicsFetchStatus,
+    ...ownProps
+});
+
+const mapDispatchToProps = dispatch => ({
+    fetchTopicsAdmin: () => dispatch(AppActions.fetchTopicsAdmin()),
+    addNotification: data => dispatch(addNotification(data))
+});
+
+export default injectIntl(routerParams(connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TopicsAdminTable)));
