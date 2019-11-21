b6f57cd0c7bbc847ce013d466a12b78a2fd1aa4c
Allen Wight <awight@redhat.com>
2019-11-21 07:44:28 -0500

    Adds topic admin component and add/edit modal
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2988

--- src/SmartComponents/Topics/Topics.js
+++ src/SmartComponents/Topics/Topics.js
@@ -1,16 +1,20 @@
-import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+
+import React from 'react';
 import asyncComponent from '../../Utilities/asyncComponent';
 
 const List = asyncComponent(() => import(/* webpackChunkName: "TopicsList" */ './List'));
 const Details = asyncComponent(() => import(/* webpackChunkName: "TopicDetails" */ './Details'));
+const Admin = asyncComponent(() => import(/* webpackChunkName: "TopicAdmin" */ '../../PresentationalComponents/TopicsAdminTable/TopicsAdminTable'));
 
 const Topics = () =>
     <React.Fragment>
         <Switch>
-            <Route exact path='/topics' component={ List } />
-            <Route exact path='/topics/:id' component={ Details }/>
-            <Redirect path='*' to='/topics'  push/>
+            <Route exact path='/topics/manage' component={Admin} />
+            <Route exact path='/topics' component={List} />
+            <Route exact path='/topics/:id' component={Details} />
+
+            <Redirect path='*' to='/topics' push />
         </Switch>
     </React.Fragment>;
 
