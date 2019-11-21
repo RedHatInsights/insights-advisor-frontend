cd19afe45594117979272b77f90c1b17380e6e69
Allen Wight <awight@redhat.com>
2019-11-21 10:18:38 -0500

    Hangs admin topics on`/topics/admin/manage` redirects if not internal (#482)
    foundational for (but doesn't fix) https://projects.engineering.redhat.com/browse/RHCLOUD-2988

--- src/SmartComponents/Topics/Topics.js
+++ src/SmartComponents/Topics/Topics.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+
 import asyncComponent from '../../Utilities/asyncComponent';
 
 const List = asyncComponent(() => import(/* webpackChunkName: "TopicsList" */ './List'));
 const Details = asyncComponent(() => import(/* webpackChunkName: "TopicDetails" */ './Details'));
+const Admin = asyncComponent(() => import(/* webpackChunkName: "TopicAdmin" */ '../../PresentationalComponents/TopicsAdminTable/TopicsAdminTable'));
 
-const Topics = () =>
-    <React.Fragment>
+const Topics = () => {
+    const [isInternal, setIsInternal] = useState({});
+    useEffect(() => { insights.chrome.auth.getUser().then((data) => setIsInternal(data.identity.internal)); }, []);
+
+    return <React.Fragment>
         <Switch>
-            <Route exact path='/topics' component={ List } />
-            <Route exact path='/topics/:id' component={ Details }/>
-            <Redirect path='*' to='/topics'  push/>
+            {isInternal && <Route exact path='/topics/admin/manage' component={Admin} />}
+            <Route exact path='/topics' component={List} />
+            <Route exact path='/topics/:id' component={Details} />
+
+            <Redirect path='*' to='/topics' push />
         </Switch>
     </React.Fragment>;
+};
 
 export default Topics;
