94cb9f6186cb45350d9ab97796dcb98d3ae51f3f
Allen Wight <awight@redhat.com>
2019-12-19 16:03:31 -0500

    Severity chart vars are translated constants, gotta dig to pull em out
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-3841

--- src/PresentationalComponents/Charts/SummaryChart/SummaryChart.js
+++ src/PresentationalComponents/Charts/SummaryChart/SummaryChart.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { invert } from 'lodash';
-import { Stack, StackItem } from '@patternfly/react-core';
-import { injectIntl } from 'react-intl';
+import './SummaryChart.scss';
 
 import { SEVERITY_MAP, TOTAL_RISK_LABEL } from '../../../AppConstants';
-import asyncComponent from '../../../Utilities/asyncComponent';
-import messages from '../../../Messages';
+import { Stack, StackItem } from '@patternfly/react-core';
 
-import './SummaryChart.scss';
+import PropTypes from 'prop-types';
+import React from 'react';
+import asyncComponent from '../../../Utilities/asyncComponent';
+import { injectIntl } from 'react-intl';
+import { invert } from 'lodash';
+import messages from '../../../Messages';
 
 const SummaryChartItem = asyncComponent(() => import('./SummaryChartItem'));
 
@@ -17,15 +17,17 @@ const SummaryChart = ({ reportsTotalRisk, rulesTotalRisk, intl }) => {
     const hitsBuilder = (rulesTotalRisk, totalIssues) => Object.entries(rulesTotalRisk).map(([key, value]) => {
         const riskName = invert(SEVERITY_MAP)[key];
         const normalizedRiskName = riskName.split('-')[0];
+        const translatedTotalRisk = (TOTAL_RISK_LABEL[key].props && TOTAL_RISK_LABEL[key].props.children) || '';
+
         if (value) {
             return <SummaryChartItem
                 key={`${riskName}-value`}
                 riskName={normalizedRiskName}
-                name={TOTAL_RISK_LABEL[key]}
+                name={translatedTotalRisk}
                 numIssues={value}
                 affectedSystems={totalIssues[key]} />;
         } else {
-            noHits.push(intl.formatMessage(messages.summaryChartItemNoHits, { severity: TOTAL_RISK_LABEL[key] }) + ' ');
+            noHits.push(intl.formatMessage(messages.summaryChartItemNoHits, { severity: translatedTotalRisk }) + ' ');
         }
     });
     const rulesWithHits = hitsBuilder(rulesTotalRisk, reportsTotalRisk).reverse();
@@ -34,14 +36,14 @@ const SummaryChart = ({ reportsTotalRisk, rulesTotalRisk, intl }) => {
         style={{ marginTop: 18 }}
         aria-label='Rule hits by severity' widget-type='InsightsSummaryChart'>
         {rulesWithHits.filter(Boolean).length > 0 ?
-            <>
+            <React.Fragment>
                 {rulesWithHits}
                 {noHits.length > 0 &&
                     <StackItem className="disabled border-top">
                         <p>{noHits}</p>
                     </StackItem>
                 }
-            </>
+            </React.Fragment>
             : <p>{intl.formatMessage(messages.summaryChartNoHits)}</p>
         }
     </Stack>;
