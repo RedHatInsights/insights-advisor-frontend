98959f6b0a0262977648997864a8327cbfba0735
Allen Wight <awight@redhat.com>
2020-01-21 11:59:19 -0500

    Topics admin as protected route - sets pattern for route rbac
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-4129

--- src/SmartComponents/Topics/Topics.js
+++ src/SmartComponents/Topics/Topics.js
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
 import asyncComponent from '../../Utilities/asyncComponent';
 
 const List = asyncComponent(() => import(/* webpackChunkName: "TopicsList" */ './List'));
 const Details = asyncComponent(() => import(/* webpackChunkName: "TopicDetails" */ './Details'));
 const Admin = asyncComponent(() => import(/* webpackChunkName: "TopicAdmin" */ '../../PresentationalComponents/TopicsAdminTable/TopicsAdminTable'));
 
-const Topics = () => {
-    const [isInternal, setIsInternal] = useState(false);
-    useEffect(() => { insights.chrome.auth.getUser().then((data) => setIsInternal(data.identity.user.is_internal)); }, []);
+const reducer = (state, { type, payload }) => ({ setLoaded: { ...state, loaded: true, isInternal: payload } }[type]);
 
-    return <React.Fragment>
-        <Switch>
-            {isInternal && <Route exact path='/topics/admin/manage' component={Admin} />}
-            <Route exact path='/topics' component={List} />
-            <Route exact path='/topics/:id' component={Details} />
+const ProtectedRoute = ({ component: Component, ...props }) => {
+    const [state, dispatch] = useReducer(reducer, { isInternal: false, loaded: false });
 
-            <Redirect path='*' to='/topics' push />
-        </Switch>
-    </React.Fragment>;
+    useEffect(() => { insights.chrome.auth.getUser().then((data) => dispatch({ type: 'setLoaded', payload: data.identity.user.is_internal })); }, []);
+
+    return <Route {...props} render={props => state.loaded && (state.isInternal ? <Component {...props} /> : <Redirect to='/topics' />)} />;
 };
+
+const Topics = () => <Switch>
+    <Route exact path='/topics' component={List} />
+    <Route exact path='/topics/:id' component={Details} />
+    <ProtectedRoute exact path='/topics/admin/manage' component={Admin} />
+
+    <Redirect path='*' to='/topics' push />
+</Switch>;
+
+ProtectedRoute.propTypes = { component: PropTypes.any };
 
 export default Topics;
