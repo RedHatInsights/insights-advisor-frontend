d4eda06d37fb884377916d6b0e6c62dfff2439b5
Allen Wight <awight@redhat.com>
2020-02-19 14:50:34 -0500

    Can't use fec getbasename package in prod, going back to our own (#560)

---
+++ src/Utilities/getBaseName.js
@@ -0,0 +1,17 @@
+const getBaseName = (pathname) => {
+    let release = '/';
+    const pathName = pathname.split('/');
+
+    pathName.shift();
+
+    if (pathName[0] === 'beta') {
+        pathName.shift();
+        release = `/beta/`;
+    }
+
+    // Most apps need this to be the first *two* elements
+    // Insights needs only one
+    return `${release}${pathName[0]}`;
+};
+
+export default getBaseName;
