6eaca868efb77566922bdedcc28e8f734da473ae
Allen Wight <awight@redhat.com>
2020-03-05 13:55:36 -0500

    Creates prod-master branch for deploying to prod-master

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,33 +2,29 @@
 set -e
 set -x
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    # for a limited time prod too
-    for env in ci qa prod
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    for env in ci qa
     do
-        echo
-        echo
         echo "PUSHING ${env}-beta"
-
-        # note to Allen.. remove the temp .git dir
-        # not all of dist O_o
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
     done
 fi
 
-if [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
-    # for a limited time prod too
-    for env in ci qa prod
+
+if [ "${TRAVIS_BRANCH}" = "master-stable" ]
+then
+    for env in ci qa
     do
-        echo
-        echo
-        echo "PUSHING ${env}-beta"
+        echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
 fi
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    echo "PUSHING ${TRAVIS_BRANCH}"
+    rm -rf ./build/.git
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
