04cdd9b8264d885ae51341eb19dbee15e13cb910
Allen Wight <awight@redhat.com>
2020-03-05 14:19:08 -0500

    Reconfigures master to push to ci/qa beta

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,31 +2,29 @@
 set -e
 set -x
 
-if [ "${TRAVIS_BRANCH}" = "master-beta" ]; then
-    rm -rf ./dist
-    BUILD_BETA=true npm run travis:build
-    for env in ci qa; do
-        echo
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    for env in ci qa
+    do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
     done
 fi
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    rm -rf ./dist
-    BUILD_BETA=true npm run travis:build
-    rm -rf ./dist/.git
-    echo
-    echo "PUSHING prod-beta"
-    .travis/release.sh "prod-beta"
-fi
-if [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
-    rm -rf ./dist
-    BUILD_BETA=true npm run travis:build
-    for env in ci qa prod; do
-        echo
+
+
+if [ "${TRAVIS_BRANCH}" = "master-stable" ]
+then
+    for env in ci qa
+    do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
+fi
+
+if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    echo "PUSHING ${TRAVIS_BRANCH}"
+    rm -rf ./build/.git
+    .travis/release.sh "${TRAVIS_BRANCH}"
 fi
