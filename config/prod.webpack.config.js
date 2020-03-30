3c96496420f66d71128a9751becbcfb4222892a5
Allen Wight <awight@redhat.com>
2020-03-30 10:09:35 -0400

    Restores missing webpack image copy  action - for Dan 🌺🥰 (#605)

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,12 +1,16 @@
 /* global require, module, __dirname */
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
-const { config: webpackConfig, plugins } = config({
-    rootFolder: resolve(__dirname, '../'),
-    debug: true
-});
+const path = require('path');
 
-module.exports = {
-    ...webpackConfig,
-    plugins
+const { config: webpackConfig, plugins } = config({ rootFolder: resolve(__dirname, '../'), debug: true });
+
+module.exports = () => {
+    const CopyFilesWebpackPlugin = new(require('copy-webpack-plugin'))([
+        { from: path.resolve(__dirname, '../static/images'), to: 'images' },
+        { from: path.resolve(__dirname, '../static/drf-yasg'), to: 'drf-yasg' },
+        { from: path.resolve(__dirname, '../static/rest_framework'), to: 'rest_framework' }]);
+    plugins.push(CopyFilesWebpackPlugin);
+
+    return { ...webpackConfig, plugins };
 };
