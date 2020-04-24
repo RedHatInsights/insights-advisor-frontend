7eebead6195aca8d1bdc34dc8583f01f2e69eeaa
Allen Wight <awight@redhat.com>
2020-04-24 11:12:36 -0400

    Use inventory when showing list of systems
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-6060

--- src/AppReducer.js
+++ src/AppReducer.js
@@ -193,6 +193,25 @@ export const AdvisorStore = (state = initialState, action) => {
     }
 };
 
+export function systemReducer(cols, INVENTORY_ACTION_TYPES) {
+    return applyReducerHash({
+        [`${INVENTORY_ACTION_TYPES.LOAD_ENTITIES}_FULFILLED`]: (state) => {
+            const { [state.columns.length - 1]: lastCol } = state.columns;
+            cols[cols.length - 1] = {
+                ...lastCol,
+                ...cols[cols.length - 1]
+            };
+            return {
+                ...state,
+                columns: cols.map(cell => ({
+                    ...cell,
+                    ...state.columns.find(({ key }) => cell.key === key)
+                }))
+            };
+        }
+    });
+}
+
 export function entitiesDetailsReducer(ActionTypes) {
     return applyReducerHash(
         {
