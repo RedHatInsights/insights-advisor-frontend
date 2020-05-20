4b37392ee6946d1d1e09270b7587c6874586f7f8
Allen Wight <awight@redhat.com>
2020-05-20 08:07:18 -0400

    wat

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -6,10 +6,16 @@ const path = require('path');
 const { config: webpackConfig, plugins } = config({ rootFolder: resolve(__dirname, '../'), debug: true });
 
 module.exports = () => {
-    const CopyFilesWebpackPlugin = new(require('copy-webpack-plugin'))([
-        { from: path.resolve(__dirname, '../static/images'), to: 'images' },
-        { from: path.resolve(__dirname, '../static/drf-yasg'), to: 'drf-yasg' },
-        { from: path.resolve(__dirname, '../static/rest_framework'), to: 'rest_framework' }]);
+    const CopyFilesWebpackPlugin = new(require('copy-webpack-plugin'))({
+        patterns: [
+            { from: path.resolve(__dirname, '../static/images'), to: 'images' },
+            { from: path.resolve(__dirname, '../static/drf-yasg'), to: 'drf-yasg' },
+            { from: path.resolve(__dirname, '../static/rest_framework'), to: 'rest_framework' }],
+
+        options: {
+            concurrency: 100
+        }
+    });
     plugins.push(CopyFilesWebpackPlugin);
 
     return { ...webpackConfig, plugins };
