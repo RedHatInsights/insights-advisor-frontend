3ce0c36dac4b6fd8eead0dd4c54d892e9b2755ee
LI0131 <mccannliam.lm@gmail.com>
2020-06-15 17:03:13 -0400

    Add Filtering to Tags Selector

--- src/PresentationalComponents/TagsToolbar/TagsToolbar.js
+++ src/PresentationalComponents/TagsToolbar/TagsToolbar.js
@@ -7,11 +7,17 @@ import { Tooltip, TooltipPosition } from '@patternfly/react-core/dist/js/compone
 import API from '../../Utilities/Api';
 import { BASE_URL } from '../../AppConstants';
 import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
+import { DEBOUNCE_DELAY } from '../../AppConstants';
+import { Divider } from '@patternfly/react-core/dist/js/components/Divider/Divider';
+import { InputGroup } from '@patternfly/react-core/dist/js/components/InputGroup/InputGroup';
 import ManageTags from './ManageTags';
 import PropTypes from 'prop-types';
+import SearchIcon from '@patternfly/react-icons/dist/js/icons/search-icon';
 import TagIcon from '@patternfly/react-icons/dist/js/icons/tag-icon';
+import { TextInput } from '@patternfly/react-core/dist/js/components/TextInput/TextInput';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
 import { connect } from 'react-redux';
+import debounce from '../../Utilities/Debounce';
 import intersection from 'lodash/intersection';
 import messages from '../../Messages';
 import { setSelectedTags } from '../../AppActions';
@@ -22,32 +28,35 @@ const TagsToolbar = ({ selectedTags, setSelectedTags }) => {
     const intl = useIntl();
     const [isOpen, setIsOpen] = useState(false);
     const [tags, setTags] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const debouncedSearchText = debounce(searchText, DEBOUNCE_DELAY);
     const [manageTagsModalOpen, setManageTagsModalOpen] = useState(false);
     const showMoreCount = 20;
-    const onToggle = isOpen => setIsOpen(isOpen);
+
+    const fetchTags = async (params, filter) => {
+        try {
+            const response = await API.get(`${BASE_URL}/tag/`);
+            filter !== '' ?
+                setTags(response.data.tags.filter(tag => tag.toLowerCase().includes(filter.toLowerCase()))) :
+                setTags(response.data.tags);
+            params === null && selectedTags === null && setSelectedTags([]);
+            if (params.length) {
+                const tagsToSet = intersection(params.split(','), response.data.tags);
+                tagUrlBuilder(tagsToSet);
+                setSelectedTags(tagsToSet);
+            }
+        } catch (error) {
+            addNotification({ variant: 'danger', dismissable: true, title: intl.formatMessage(messages.error), description: `${error}` });
+        }
+    };
 
     useEffect(() => {
         const url = new URL(window.location);
         let params = new URLSearchParams(url.search);
         params = params.get('tags');
-        const fetchTags = async () => {
-            try {
-                const response = await API.get(`${BASE_URL}/tag/`);
-                setTags(response.data.tags);
-                params === null && selectedTags === null && setSelectedTags([]);
-                if (params.length) {
-                    const tagsToSet = intersection(params.split(','), response.data.tags);
-                    tagUrlBuilder(tagsToSet);
-                    setSelectedTags(tagsToSet);
-                }
-            } catch (error) {
-                addNotification({ variant: 'danger', dismissable: true, title: intl.formatMessage(messages.error), description: `${error}` });
-            }
-        };
-
-        fetchTags();
+        fetchTags(params, searchText);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [debouncedSearchText]);
 
     useEffect(() => {
         tagUrlBuilder(selectedTags);
@@ -62,6 +71,11 @@ const TagsToolbar = ({ selectedTags, setSelectedTags }) => {
             </React.Fragment>
             : intl.formatMessage(messages.noTags)}
     </React.Fragment>;
+
+    const onToggle = isOpen => {
+        setSearchText();
+        setIsOpen(isOpen);
+    };
 
     const onSelect = (e, selection) => {
         const tagsToSet = selectedTags.includes(selection) ? selectedTags.filter(item => item !== selection)
@@ -88,6 +102,15 @@ const TagsToolbar = ({ selectedTags, setSelectedTags }) => {
             ariaLabelledBy='select-group-input'
             isDisabled={tags.length === 0}
         >
+            <InputGroup className='tags-filter-group'>
+                <TextInput
+                    placeholder={intl.formatMessage(messages.filterTagsInToolbar)}
+                    value={searchText}
+                    onChange={setSearchText}
+                />
+                <SearchIcon className='tags-filter-search-icon'/>
+            </InputGroup>
+            <Divider key="inline-filter-divider" />
             {tags.slice(0, showMoreCount || tags.length).map(item =>
                 <SelectOption key={item} value={`${item}`}>
                     <Tooltip content={`${decodeURIComponent(item)}`} position={TooltipPosition.right}>
