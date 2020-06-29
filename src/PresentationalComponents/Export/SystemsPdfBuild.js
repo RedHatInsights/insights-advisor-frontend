b1471b95429485bd75ff6333288362ff3f38f855
Allen Wight <awight@redhat.com>
2020-06-29 08:16:40 -0400

    asdf

--- src/PresentationalComponents/Export/SystemsPdfBuild.js
+++ src/PresentationalComponents/Export/SystemsPdfBuild.js
@@ -1,18 +1,30 @@
 /* eslint-disable camelcase */
 import { Column, Section, Table } from '@redhat-cloud-services/frontend-components-pdf-generator';
+import { Link, StyleSheet, Text } from '@react-pdf/renderer';
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Text } from '@react-pdf/renderer';
+import global_FontWeight_bold from '@patternfly/react-tokens/dist/js/global_FontWeight_bold';
+import global_link_Color from '@patternfly/react-tokens/dist/js/global_link_Color';
+import global_spacer_md from '@patternfly/react-tokens/dist/js/global_spacer_md';
 import messages from '../../Messages';
+
+const styles = StyleSheet.create({
+    bold: { fontWeight: global_FontWeight_bold.value },
+    link: { color: global_link_Color.value },
+    text: { marginTop: global_spacer_md.value  }
+});
 
 export const tablePage = ({ page, systems, intl }) => {
     const rowHeaders = [intl.formatMessage(messages.name), intl.formatMessage(messages.recommendations), intl.formatMessage(messages.critical),
         intl.formatMessage(messages.important), intl.formatMessage(messages.moderate), intl.formatMessage(messages.low),
         intl.formatMessage(messages.lastSeen)];
     const rows = [
-        ...systems.map(system => [system.display_name, `${system.hits}`, `${system.critical_hits}`, `${system.important_hits}`,
-            `${system.moderate_hits}`, `${system.low_hits}`, new Date(system.last_seen).toUTCString()
+        ...systems.map(system => [
+            <Link key={system.system_uuid} style={styles.link}
+                src={`https://cloud.redhat.com/insights/advisor/systems/${system.system_uuid}/`}>system.display_name</Link>,
+            `${system.hits}`, `${system.critical_hits}`, `${system.important_hits}`, `${system.moderate_hits}`, `${system.low_hits}`,
+            new Date(system.last_seen).toUTCString()
         ])];
 
     return <React.Fragment key={page}>
@@ -30,24 +42,24 @@ tablePage.propTypes = {
 
 export const leadPage = ({ systemsTotal, systems, filters, tags, intl }) => {
     return <React.Fragment key={`${intl.formatMessage(messages.insightsHeader)}: ${intl.formatMessage(messages.systems)}`}>
-        <Text key='sys-count'>
+        <Text key='sys-count' style={styles.text}>
             {intl.formatMessage(messages.sysTableCount, {
-                systems: <Text style={{ fontWeight: 700 }}>
+                systems: <Text key='sys-count-count' style={styles.bold}>
                     {intl.formatMessage(messages.execReportHeaderSystems, { systems: systemsTotal })}
                 </Text>
             })}
         </Text>
-        <Text key='filters-applied'>
+        <Text key='sys-filters' style={styles.text}>
             {intl.formatMessage(messages.filtersApplied)}
         </Text>
-        <Text>
+        <Text key='sys-filters-values' style={styles.bold}>
             {Object.entries(filters).map(value => <Text key={value}>{`${value[0]}: ${value[1]} `}</Text>)}
         </Text>
-        <Text key='tags-applied'>
+        <Text key='sys-tags' style={styles.text}>
             {intl.formatMessage(messages.tagsApplied)}
         </Text>
-        <Text>{tags ? decodeURIComponent(tags) : intl.formatMessage(messages.noTags)}</Text>
-        <Section title='Systems'>
+        <Text key='sys-tags-values' style={styles.bold}>{tags ? decodeURIComponent(tags) : intl.formatMessage(messages.noTags)}</Text>
+        <Section key='systems' title='Systems'>
             {tablePage({ systems, intl })}
         </Section>
     </React.Fragment >;
