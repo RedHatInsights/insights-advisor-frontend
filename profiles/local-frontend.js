52511d3fae0080f61169f525c7643abd862744e2
Allen Wight <awight@redhat.com>
2020-09-08 09:54:29 -0400

    Fixes global filter tagging, removes hardcoded chrome source

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,13 +1,11 @@
 /*global module*/
 const apiHost = `https://cloud.redhat.com`;
 const frontendHost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
-const chromeHost = 'https://cloud.redhat.com';
 const SECTION = 'insights';
 const APP_ID = 'advisor';
 const routes = {};
 
 routes[`/api/${SECTION}/v1/`] = { host: apiHost };
-routes['/apps/chrome'] = { host: chromeHost };
 routes[`/apps/${SECTION}`] = { host: `http://${frontendHost}:8002` };
 routes[`/apps/${APP_ID}`] = { host: `http://${frontendHost}:8002` };
 routes[`/beta/apps/${APP_ID}`] = { host: `http://${frontendHost}:8002` };
