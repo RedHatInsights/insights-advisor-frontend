48374812aa99f0495af9f90075edc57db841988c
Allen Wight <awight@redhat.com>
2021-04-13 15:45:05 -0400

    Topics filter value to lowercase (#809)
    fixes https://issues.redhat.com/browse/ADVISOR-1620

--- src/PresentationalComponents/TopicsTable/TopicsTable.js
+++ src/PresentationalComponents/TopicsTable/TopicsTable.js
@@ -68,7 +68,7 @@ const TopicsTable = () => {
     }, [topics, searchText]);
 
     const filterConfigItems = [{
-        label: intl.formatMessage(messages.name),
+        label: intl.formatMessage(messages.name).toLowerCase(),
         filterValues: {
             key: 'text-filter',
             onChange: (event, value) => { setSearchText(value); setSort({}); },
