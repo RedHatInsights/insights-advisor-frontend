e2aa48cce74bf169dfa18e89624d8950456440fd
Allen Wight <awight@redhat.com>
2021-06-04 08:53:49 -0400

    Removes rh disabled rules alert from rec page (#827)
    fixes https://issues.redhat.com/browse/ADVISOR-1778

--- src/SmartComponents/Recs/List.js
+++ src/SmartComponents/Recs/List.js
@@ -1,18 +1,15 @@
 import './List.scss';
 
-import { PERMS, RULES_FETCH_URL } from '../../AppConstants';
 import {
   PageHeader,
   PageHeaderTitle,
 } from '@redhat-cloud-services/frontend-components/PageHeader';
-import React, { Suspense, lazy, useEffect, useState } from 'react';
+import React, { Suspense, lazy } from 'react';
 
-import API from '../../Utilities/Api';
-import { Alert } from '@patternfly/react-core/dist/esm/components/Alert/Alert';
-import { AlertActionCloseButton } from '@patternfly/react-core/dist/esm/components/Alert/AlertActionCloseButton';
 import DownloadExecReport from '../../PresentationalComponents/ExecutiveReport/Download';
 import Loading from '../../PresentationalComponents/Loading/Loading';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
+import { PERMS } from '../../AppConstants';
 import { Tooltip } from '@patternfly/react-core/dist/esm/components/Tooltip/Tooltip';
 import messages from '../../Messages';
 import { useIntl } from 'react-intl';
@@ -27,31 +24,9 @@ const RulesTable = lazy(() =>
 const List = () => {
   const intl = useIntl();
   const permsExport = usePermissions('advisor', PERMS.export);
-  const AdvisorRedHatDisabledRuleAlert = JSON.parse(
-    localStorage.getItem('AdvisorRedHatDisabledRuleAlert') || 'true'
-  );
-  const [redhatDisabledRuleAlert, setRedHatDisabledRuleAlert] = useState(
-    AdvisorRedHatDisabledRuleAlert
-  );
-  const [redHatDisabledRuleCount, setRedHatDisabledRuleCount] = useState(0);
-
   document.title = intl.formatMessage(messages.documentTitle, {
     subnav: messages.recommendations.defaultMessage,
   });
-
-  useEffect(() => {
-    AdvisorRedHatDisabledRuleAlert &&
-      (async () => {
-        try {
-          const response = (
-            await API.get(`${RULES_FETCH_URL}?rule_status=rhdisabled`)
-          ).data;
-          setRedHatDisabledRuleCount(response.meta.count);
-        } catch (error) {
-          setRedHatDisabledRuleCount(0);
-        }
-      })();
-  }, [AdvisorRedHatDisabledRuleAlert]);
 
   return (
     <React.Fragment>
@@ -68,27 +43,6 @@ const List = () => {
           >
             <DownloadExecReport isDisabled={!permsExport.hasAccess} />
           </Tooltip>
-        )}
-        {redhatDisabledRuleAlert && redHatDisabledRuleCount > 0 && (
-          <Alert
-            className="alertOverride"
-            variant="warning"
-            isInline
-            title={intl.formatMessage(messages.redhatDisabledRuleAlertTitle)}
-            actionClose={
-              <AlertActionCloseButton
-                onClose={() => {
-                  setRedHatDisabledRuleAlert(false);
-                  localStorage.setItem(
-                    'AdvisorRedHatDisabledRuleAlert',
-                    'false'
-                  );
-                }}
-              />
-            }
-          >
-            {intl.formatMessage(messages.redhatDisabledRuleAlert)}
-          </Alert>
         )}
       </PageHeader>
       <Main>
