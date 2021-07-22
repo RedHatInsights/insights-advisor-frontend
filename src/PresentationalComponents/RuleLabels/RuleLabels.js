c740a1bca848b8ecce21b1af50fe975e138ca338
Allen Wight <awight@redhat.com>
2021-07-22 13:35:27 -0400

    Adds categories component and adds it to rec table 🤯
    fixes https://issues.redhat.com/browse/ADVISOR-1880
    fixes https://issues.redhat.com/browse/ADVISOR-1894

--- src/PresentationalComponents/RuleLabels/RuleLabels.js
+++ src/PresentationalComponents/RuleLabels/RuleLabels.js
@@ -8,45 +8,47 @@ import {
 import { Label } from '@patternfly/react-core/dist/js/components/Label/Label';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { injectIntl } from 'react-intl';
 import messages from '../../Messages';
+import { useIntl } from 'react-intl';
 
-const RuleLabels = ({ intl, rule }) => (
-  <React.Fragment>
-    {rule?.tags?.search('incident') !== -1 && (
-      <Tooltip
-        content={intl.formatMessage(messages.incidentTooltip)}
-        position={TooltipPosition.right}
-      >
-        <Label color="red" className="incidentLabel">
-          {intl.formatMessage(messages.incident)}
-        </Label>
-      </Tooltip>
-    )}
-    {rule.rule_status === 'disabled' && (
-      <Tooltip
-        content={intl.formatMessage(messages.ruleIsDisabledTooltip)}
-        position={TooltipPosition.right}
-      >
-        <Label color="gray">{intl.formatMessage(messages.disabled)}</Label>
-      </Tooltip>
-    )}
-    {rule.rule_status === 'rhdisabled' && (
-      <Tooltip
-        content={intl.formatMessage(messages.ruleIsDisabledTooltip)}
-        position={TooltipPosition.right}
-      >
-        <Label color="gray">
-          {intl.formatMessage(messages.redhatDisabled)}
-        </Label>
-      </Tooltip>
-    )}
-  </React.Fragment>
-);
+const RuleLabels = ({ rule }) => {
+  const intl = useIntl();
+  return (
+    <React.Fragment>
+      {rule?.tags?.search('incident') !== -1 && (
+        <Tooltip
+          content={intl.formatMessage(messages.incidentTooltip)}
+          position={TooltipPosition.right}
+        >
+          <Label color="red" className="incidentLabel">
+            {intl.formatMessage(messages.incident)}
+          </Label>
+        </Tooltip>
+      )}
+      {rule.rule_status === 'disabled' && (
+        <Tooltip
+          content={intl.formatMessage(messages.ruleIsDisabledTooltip)}
+          position={TooltipPosition.right}
+        >
+          <Label color="gray">{intl.formatMessage(messages.disabled)}</Label>
+        </Tooltip>
+      )}
+      {rule.rule_status === 'rhdisabled' && (
+        <Tooltip
+          content={intl.formatMessage(messages.ruleIsDisabledTooltip)}
+          position={TooltipPosition.right}
+        >
+          <Label color="gray">
+            {intl.formatMessage(messages.redhatDisabled)}
+          </Label>
+        </Tooltip>
+      )}
+    </React.Fragment>
+  );
+};
 
 RuleLabels.propTypes = {
-  intl: PropTypes.any,
   rule: PropTypes.object,
 };
 
-export default injectIntl(RuleLabels);
+export default RuleLabels;
