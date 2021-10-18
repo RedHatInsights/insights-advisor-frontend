575614480e615f2d4e92569ead4f78b4dc142e31
Allen Wight <awight@redhat.com>
2021-10-18 14:51:49 -0400

    Add chart to Total risk of pathway
    fixes https://issues.redhat.com/browse/ADVISOR-1891

--- src/PresentationalComponents/Cards/Pathways.js
+++ src/PresentationalComponents/Cards/Pathways.js
@@ -6,6 +6,13 @@ import {
   CardTitle,
 } from '@patternfly/react-core/dist/esm/components/Card/index';
 import {
+  Chart,
+  ChartAxis,
+  ChartBar,
+  ChartGroup,
+  ChartVoronoiContainer,
+} from '@patternfly/react-charts';
+import {
   Grid,
   GridItem,
 } from '@patternfly/react-core/dist/esm/layouts/Grid/index';
@@ -20,6 +27,10 @@ import { RebootRequired } from '../Common/Common';
 import RecommendationLevel from '../Labels/RecommendationLevel';
 import RuleLabels from '../Labels/RuleLabels';
 import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
+import chart_color_black_100 from '@patternfly/react-tokens/dist/esm/chart_color_black_100';
+import chart_color_gold_400 from '@patternfly/react-tokens/dist/esm/chart_color_gold_400';
+import chart_color_orange_300 from '@patternfly/react-tokens/dist/esm/chart_color_orange_300';
+import chart_color_red_100 from '@patternfly/react-tokens/dist/esm/chart_color_red_100';
 import messages from '../../Messages';
 import { strong } from '../../Utilities/intlHelper';
 import { useIntl } from 'react-intl';
@@ -66,7 +77,15 @@ const PathwayCard = (props) => {
 };
 const TotalRisk = (props) => {
   const intl = useIntl();
-  const { impacted_systems_count, incident_count, categories } = props;
+  const {
+    impacted_systems_count,
+    incident_count,
+    categories,
+    critical_risk_count,
+    high_risk_count,
+    medium_risk_count,
+    low_risk_count,
+  } = props;
 
   const catString = (cats) =>
     cats.length > 1 ? categories.map((cat) => cat.name).join(', ') : cats.name;
@@ -79,7 +98,66 @@ const TotalRisk = (props) => {
       <CardTitle>{intl.formatMessage(messages.totalRiskPathway)}</CardTitle>
       <CardBody className={`body`}>
         <Grid>
-          <GridItem span={6}> insert chart</GridItem>
+          <GridItem span={6}>
+            <div>
+              <Chart
+                ariaDesc="Total risk of recommendations"
+                ariaTitle="Total risk of recommendations"
+                containerComponent={
+                  <ChartVoronoiContainer
+                    labels={({ datum }) => `${datum.name}: ${datum.y}`}
+                    constrainToVisibleArea
+                  />
+                }
+                height={300}
+                width={300}
+                padding={{
+                  bottom: 30,
+                  left: 35,
+                  right: 20,
+                  top: 10,
+                }}
+              >
+                <ChartAxis />
+                <ChartAxis dependentAxis showGrid />
+                <ChartGroup>
+                  <ChartBar
+                    style={{
+                      data: {
+                        fill: ({ datum }) => datum.fill,
+                      },
+                    }}
+                    data={[
+                      {
+                        name: 'Critical',
+                        x: 'Critical',
+                        y: critical_risk_count,
+                        fill: chart_color_red_100.value,
+                      },
+                      {
+                        name: 'Important',
+                        x: 'Important',
+                        y: high_risk_count,
+                        fill: chart_color_orange_300.value,
+                      },
+                      {
+                        name: 'Moderate',
+                        x: 'Moderate',
+                        y: medium_risk_count,
+                        fill: chart_color_gold_400.value,
+                      },
+                      {
+                        name: 'Low',
+                        x: 'Low',
+                        y: low_risk_count,
+                        fill: chart_color_black_100.value,
+                      },
+                    ]}
+                  />
+                </ChartGroup>
+              </Chart>
+            </div>
+          </GridItem>
           <GridItem span={6}>
             {intl.formatMessage(messages.thisPathway, {
               category: catString(categories),
