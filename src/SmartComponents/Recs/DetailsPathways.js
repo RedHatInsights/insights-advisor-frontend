7723265f7cc828320f2c6fac2f62389b6a6ad0ff
Gabi Podolnikova <gpodolni@redhat.com>
2022-03-16 11:11:36 +0100

    Adding tag filtering to all inventory tables

--- src/SmartComponents/Recs/DetailsPathways.js
+++ src/SmartComponents/Recs/DetailsPathways.js
@@ -180,6 +180,7 @@ const PathwayDetails = () => {
                   selectedTags={selectedTags}
                   workloads={workloads}
                   SID={SID}
+                  showTags={true}
                 />
               </Suspense>
             )}
