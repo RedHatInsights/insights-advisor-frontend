5d22d907bca4386243e4e1dabd8666245c861306
Sebastian Gräßl <bastilian@users.noreply.github.com>
2022-10-21 17:51:12 +0200

    refactor(AppReducer): ESSNTL-3505 - Remove unneeded last column mangling (#1037)

--- src/Store/AppReducer.js
+++ src/Store/AppReducer.js
@@ -1,22 +1,8 @@
 import Advisor from '../SmartComponents/SystemAdvisor';
 import { applyReducerHash } from '@redhat-cloud-services/frontend-components-utilities/ReducerRegistry';
 
-export function systemReducer(cols, INVENTORY_ACTION_TYPES) {
+export function systemReducer() {
   return applyReducerHash({
-    [`${INVENTORY_ACTION_TYPES.LOAD_ENTITIES}_FULFILLED`]: (state) => {
-      const { [state.columns.length - 1]: lastCol } = state.columns;
-      cols[cols.length - 1] = {
-        ...lastCol,
-        ...cols[cols.length - 1],
-      };
-      return {
-        ...state,
-        columns: cols.map((cell) => ({
-          ...cell,
-          ...state.columns.find(({ key }) => cell.key === key),
-        })),
-      };
-    },
     ['SELECT_ENTITIES']: (state, { payload: { selected } }) => ({
       ...state,
       rows: selectRows(state.rows, selected),
