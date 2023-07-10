a919a089348aa0e9f2fa8b4f75dabcb11ca8a7e6
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-07-10 12:23:38 +0200

    chore: refactor pdf-generator migration to use shared FEC hook

--- src/PresentationalComponents/ExecutiveReport/Download.js
+++ src/PresentationalComponents/ExecutiveReport/Download.js
@@ -1,40 +1,26 @@
 import './_Download.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux/actions/notifications';
 import ExportIcon from '@patternfly/react-icons/dist/esm/icons/export-icon';
 import messages from '../../Messages';
 import { useIntl } from 'react-intl';
 import { Button } from '@patternfly/react-core';
-import { fetchPDFReport } from '../../Utilities/Api';
-import { renderPDF } from '../../Utilities/Helpers';
-import { exportNotifications } from '../../AppConstants';
+import usePDFExport from '@redhat-cloud-services/frontend-components-utilities/useExportPDF/useExportPDF';
 
 const DownloadExecReport = ({ isDisabled }) => {
   const intl = useIntl();
-  const dispatch = useDispatch();
+  const downloadReport = usePDFExport('advisor');
+  const fileName = `Advisor-Executive-Report--${new Date()
+    .toUTCString()
+    .replace(/ /g, '-')}.pdf`;
 
-  const dataFetch = async () => {
-    dispatch(addNotification(exportNotifications.pending));
-
-    try {
-      const result = await fetchPDFReport('advisor');
-      renderPDF(
-        result,
-        `Advisor-Executive-Report--${new Date()
-          .toUTCString()
-          .replace(/ /g, '-')}.pdf`
-      );
-      dispatch(addNotification(exportNotifications.success));
-    } catch (e) {
-      dispatch(addNotification(exportNotifications.error));
-    }
+  const downloadPDFReport = () => {
+    downloadReport('advisor', fileName);
   };
 
   return (
     <Button
-      onClick={dataFetch}
+      onClick={downloadPDFReport}
       variant="link"
       isInline
       isDisabled={isDisabled}
@@ -49,4 +35,5 @@ const DownloadExecReport = ({ isDisabled }) => {
 DownloadExecReport.propTypes = {
   isDisabled: PropTypes.bool,
 };
+
 export default DownloadExecReport;
