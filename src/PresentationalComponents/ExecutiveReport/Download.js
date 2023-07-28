6a54de2df40fc25fdc59ee8634687b69b5d4f35d
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-07-28 11:29:40 +0200

    Revert "chore: refactor pdf-generator migration to use shared FEC hook (#1149)"
    This reverts commit d688ae0c3845c79a9ccd190019a6f914e6b7b87e.

--- src/PresentationalComponents/ExecutiveReport/Download.js
+++ src/PresentationalComponents/ExecutiveReport/Download.js
@@ -1,28 +1,40 @@
 import './_Download.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux/actions/notifications';
 import ExportIcon from '@patternfly/react-icons/dist/esm/icons/export-icon';
 import messages from '../../Messages';
 import { useIntl } from 'react-intl';
 import { Button } from '@patternfly/react-core';
-import usePDFExport from '@redhat-cloud-services/frontend-components-utilities/useExportPDF/useExportPDF';
-import { useDispatch } from 'react-redux';
+import { fetchPDFReport } from '../../Utilities/Api';
+import { renderPDF } from '../../Utilities/Helpers';
+import { exportNotifications } from '../../AppConstants';
 
 const DownloadExecReport = ({ isDisabled }) => {
   const intl = useIntl();
   const dispatch = useDispatch();
-  const downloadReport = usePDFExport('advisor', dispatch);
-  const fileName = `Advisor-Executive-Report--${new Date()
-    .toUTCString()
-    .replace(/ /g, '-')}.pdf`;
 
-  const downloadPDFReport = () => {
-    downloadReport('advisor', fileName);
+  const dataFetch = async () => {
+    dispatch(addNotification(exportNotifications.pending));
+
+    try {
+      const result = await fetchPDFReport('advisor');
+      renderPDF(
+        result,
+        `Advisor-Executive-Report--${new Date()
+          .toUTCString()
+          .replace(/ /g, '-')}.pdf`
+      );
+      dispatch(addNotification(exportNotifications.success));
+    } catch (e) {
+      dispatch(addNotification(exportNotifications.error));
+    }
   };
 
   return (
     <Button
-      onClick={downloadPDFReport}
+      onClick={dataFetch}
       variant="link"
       isInline
       isDisabled={isDisabled}
@@ -37,5 +49,4 @@ const DownloadExecReport = ({ isDisabled }) => {
 DownloadExecReport.propTypes = {
   isDisabled: PropTypes.bool,
 };
-
 export default DownloadExecReport;
