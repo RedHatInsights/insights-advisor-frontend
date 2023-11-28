1916cac2fd183ba6f44561414ba7db35841a0edd
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-11-28 12:42:41 +0100

    chore(RHINENG-3070): remove useEffect to clear redux from PathwaySystems

--- src/SmartComponents/HybridInventoryTabs/ConventionalSystems/PathwaySystems.js
+++ src/SmartComponents/HybridInventoryTabs/ConventionalSystems/PathwaySystems.js
@@ -1,34 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
 import Inventory from '../../../PresentationalComponents/Inventory/Inventory';
 
-const PathwaySystems = ({ pathway, selectedTags, workloads, SID }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    return () => {
-      dispatch({
-        type: 'CLEAR_INVENTORY_STORE',
-        payload: [],
-      });
-    };
-  }, [dispatch]);
-
-  return (
-    <Inventory
-      tableProps={{
-        canSelectAll: false,
-        isStickyHeader: true,
-      }}
-      pathway={pathway}
-      selectedTags={selectedTags}
-      workloads={workloads}
-      SID={SID}
-      showTags
-    />
-  );
-};
+const PathwaySystems = ({ pathway, selectedTags, workloads, SID }) => (
+  <Inventory
+    tableProps={{
+      canSelectAll: false,
+      isStickyHeader: true,
+    }}
+    pathway={pathway}
+    selectedTags={selectedTags}
+    workloads={workloads}
+    SID={SID}
+    showTags
+  />
+);
 
 PathwaySystems.propTypes = {
   pathway: PropTypes.object,
