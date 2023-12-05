478e2d2faac6a8928c1b9a8249d2adb8a393476c
Muslimjon <59481011+mkholjuraev@users.noreply.github.com>
2023-12-05 21:13:12 +0100

    feat(RHINENG-5357): modify systems page to show only conventional systems (#1212)

--- src/PresentationalComponents/Loading/Loading.js
+++ src/PresentationalComponents/Loading/Loading.js
@@ -1,5 +1,5 @@
-import { Card } from '@patternfly/react-core/dist/esm/components/Card/Card';
-import { CardBody } from '@patternfly/react-core/dist/esm/components/Card/CardBody';
+import { Card } from '@patternfly/react-core';
+import { CardBody } from '@patternfly/react-core';
 import { List } from 'react-content-loader';
 import React from 'react';
 const Loading = () => (
