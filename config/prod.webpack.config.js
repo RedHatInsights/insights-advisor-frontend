9c6fe12254af1d1766cfe23edea130b9ba4d19f7
Adonis Puente <adonis.puente@gmail.com>
2024-04-16 17:01:39 -0400

    feat(sentryConfig): Adds sentry configuration

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -2,6 +2,8 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const path = require('path');
+const webpack = require('webpack');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
@@ -23,9 +25,24 @@ plugins.push(
         './RootApp': resolve(__dirname, '../src/AppEntry'),
         './SystemDetail': resolve(__dirname, '../src/Modules/SystemDetail'),
       },
-    }
+    },
+    ...(process.env.SENTRY_AUTH_TOKEN
+      ? sentryWebpackPlugin({
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          org: process.env.SENTRY_ORG,
+          project: process.env.SENTRY_PPROJECT,
+        })
+      : {})
   )
 );
+
+const SourceMapsPlugin = new webpack.SourceMapDevToolPlugin({
+  test: /\.js/i,
+  exclude: /(node_modules|bower_components)/i,
+  filename: `sourcemaps/[name].js.map`,
+});
+
+plugins.push(SourceMapsPlugin);
 
 module.exports = () => {
   const CopyFilesWebpackPlugin = new (require('copy-webpack-plugin'))({
@@ -37,7 +54,8 @@ module.exports = () => {
       },
     ],
   });
-  plugins.push(CopyFilesWebpackPlugin);
+  'source-map', // Source map generation must be turned on
+    plugins.push(CopyFilesWebpackPlugin);
 
   return { ...webpackConfig, plugins };
 };
