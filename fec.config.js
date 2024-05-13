732e06974cbf6fffe72a5ca4a2172ff3f8d08b04
Fewwy <avoznese@redhat.com>
2024-05-13 17:16:45 +0200

    added sentry plugin and removed copy webpack plugin

--- fec.config.js
+++ fec.config.js
@@ -1,11 +1,19 @@
 const { resolve } = require('path');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
   appUrl: '/insights/advisor',
   debug: true,
   useProxy: process.env.PROXY === 'true',
   proxyVerbose: true,
-  plugins: [],
+  plugins: [
+    process.env.SENTRY_AUTH_TOKEN &&
+      sentryWebpackPlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: process.env.SENTRY_ORG,
+        project: process.env.SENTRY_PROJECT,
+      }),
+  ],
   ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
   moduleFederation: {
     shared: [
