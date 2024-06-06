16fd12a92595d5f943aed76f453afe2c8d1f5c69
Muslimjon <mkholjur@redhat.com>
2024-06-06 12:04:41 +0200

    chore: test commit, that should not be merged

---
+++ src/SmartComponents/TestComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TestComponent = () => (
+  <article>
+    <h1>My First Component</h1>
+    <ol>
+      <li>Components: UI Building Blocks</li>
+      <li>Defining a Component</li>
+      <li>Using a Component</li>
+    </ol>
+  </article>
+);
+
+export default TestComponent;
