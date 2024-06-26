ba7fc9bcb410c298aba483f0933695f7c6aff724
Martin Michalik <xmicha82@vutbr.cz>
2024-06-26 12:46:59 +0200

    fix(ZeroStateWrapper): RHINENG-10312 dont show ZeroState on data loading

---
+++ src/ZeroStateWrapper.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
+import { ZeroStateWrapper } from './ZeroStateWrapper';
+import { useGetConventionalDevicesQuery } from './Services/SystemVariety';
+import { ComponentWithContext } from './Utilities/TestingUtilities';
+
+jest.mock('./Utilities/Hooks', () => ({
+  ...jest.requireActual('./Utilities/Hooks'),
+  useFeatureFlag: jest.fn(() => false),
+}));
+
+jest.mock('./Services/SystemVariety', () => ({
+  ...jest.requireActual('./Services/SystemVariety'),
+  useGetConventionalDevicesQuery: jest.fn(() => ({
+    data: {
+      total: 5,
+    },
+    isSuccess: true,
+    isError: false,
+  })),
+  useGetEdgeDevicesQuery: jest.fn(() => ({
+    data: {
+      total: 5,
+    },
+    isSuccess: true,
+    isError: false,
+  })),
+}));
+
+const noConvSystems = {
+  data: {
+    total: 0,
+  },
+  isSuccess: true,
+  isError: false,
+};
+
+const notLoaded = {
+  data: {
+    total: 0,
+  },
+  isSuccess: false,
+  isError: false,
+};
+
+describe('ZeroStateWrapper', () => {
+  it('Should render children with conventional devices', () => {
+    render(
+      <ComponentWithContext
+        Component={ZeroStateWrapper}
+        componentProps={{ children: <div>Rendered</div> }}
+      />
+    );
+
+    expect(screen.getByText('Rendered'));
+  });
+
+  it('Should render ZeroState with no systems', () => {
+    useGetConventionalDevicesQuery.mockReturnValue(noConvSystems);
+    render(
+      <ComponentWithContext
+        Component={ZeroStateWrapper}
+        componentProps={{ children: <div>Rendered</div> }}
+      />
+    );
+
+    expect(screen.getByText('AsyncComponent'));
+  });
+
+  it('Should not render ZeroState when waiting for data to load', () => {
+    useGetConventionalDevicesQuery.mockReturnValue(notLoaded);
+    render(
+      <ComponentWithContext
+        Component={ZeroStateWrapper}
+        componentProps={{ children: <div>Rendered</div> }}
+      />
+    );
+
+    expect(screen.getByText('Rendered'));
+  });
+});
