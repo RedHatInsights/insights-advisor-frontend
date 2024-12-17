d3908a2092fea048a56bbdf604dc9e2d3b254589
Adonis Puente <adonis.puente@gmail.com>
2024-12-17 10:49:11 -0600

    refactor(routes): Fix zero state flash and add missing routes

--- src/Routes.js
+++ src/Routes.js
@@ -19,76 +19,72 @@ import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComp
 import { Spinner } from '@patternfly/react-core';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux/actions/notifications';
 
-const routes = {
-  recsList: {
-    path: 'recommendations',
+const routeConfig = [
+  {
+    key: 'recsList',
+    paths: ['recommendations', 'recommendations/pathways'],
+    component: lazy(() => import('./SmartComponents/Recs/List')),
+  },
+  {
+    key: 'recsDetails',
+    paths: ['recommendations/:id'],
+    component: lazy(() => import('./SmartComponents/Recs/Details')),
+  },
+  {
+    key: 'recsDetailsImmutable',
+    paths: ['recommendations/:id/manage-edge-inventory'],
+    component: lazy(() => import('./SmartComponents/Recs/Details')),
+    props: { isImmutableTabOpen: true },
+  },
+  {
+    key: 'detailsPathways',
+    paths: [
+      'recommendations/pathways/:id',
+      'recommendations/pathways/systems/:id',
+    ],
+    component: lazy(() => import('./SmartComponents/Recs/DetailsPathways')),
+  },
+  {
+    key: 'detailsPathwaysImmutable',
+    paths: ['recommendations/pathways/:id/manage-edge-inventory'],
+    component: lazy(() => import('./SmartComponents/Recs/DetailsPathways')),
+    props: { isImmutableTabOpen: true },
+  },
+  {
+    key: 'inventoryDetails',
+    paths: [
+      'recommendations/:id/:inventoryId/',
+      'recommendations/pathways/systems/:id/:inventoryId/',
+      'recommendations/pathways/:id/:inventoryId/',
+      'systems/:inventoryId/',
+    ],
     component: lazy(() =>
-      import(/* webpackChunkName: "RecsList" */ './SmartComponents/Recs/List')
+      import('./PresentationalComponents/Inventory/InventoryDetails')
     ),
   },
-  recsDetails: {
-    path: 'recommendations/:id',
+  {
+    key: 'systemsList',
+    paths: ['systems'],
+    component: lazy(() => import('./SmartComponents/Systems/List')),
+  },
+  {
+    key: 'topicsList',
+    paths: ['topics'],
+    component: lazy(() => import('./SmartComponents/Topics/List')),
+  },
+  {
+    key: 'topicDetails',
+    paths: ['topics/:id'],
+    component: lazy(() => import('./SmartComponents/Topics/Details')),
+  },
+  {
+    key: 'topicAdmin',
+    paths: ['topics/admin/manage'],
     component: lazy(() =>
-      import(
-        /* webpackChunkName: "RecsDetails" */ './SmartComponents/Recs/Details'
-      )
+      import('./PresentationalComponents/TopicsAdminTable/TopicsAdminTable')
     ),
   },
-  recsDetailsPathways: {
-    path: 'recommendations/pathways/:id',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "Details-Pathways" */ './SmartComponents/Recs/DetailsPathways'
-      )
-    ),
-  },
-  inventoryDetails: {
-    path: 'recommendations/:id/:inventoryId',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "InventoryDetails" */ './PresentationalComponents/Inventory/InventoryDetails'
-      )
-    ),
-  },
-  systemsList: {
-    path: 'systems',
-    component: lazy(() =>
-      import(/* webpackChunkName: "List" */ './SmartComponents/Systems/List')
-    ),
-  },
-  systemsDetails: {
-    path: 'systems/:inventoryId',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "InventoryDetails" */ './PresentationalComponents/Inventory/InventoryDetails'
-      )
-    ),
-  },
-  topicsList: {
-    path: 'topics',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "TopicsList" */ './SmartComponents/Topics/List'
-      )
-    ),
-  },
-  topicDetails: {
-    path: 'topics/:id',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "TopicDetails" */ './SmartComponents/Topics/Details'
-      )
-    ),
-  },
-  topicAdmin: {
-    path: '/topics/admin/manage',
-    component: lazy(() =>
-      import(
-        /* webpackChunkName: "TopicAdmin" */ './PresentationalComponents/TopicsAdminTable/TopicsAdminTable'
-      )
-    ),
-  },
-};
+];
 
 export const AccountStatContext = createContext({
   hasConventionalSystems: true,
@@ -101,11 +97,14 @@ export const AdvisorRoutes = () => {
   const isEdgeParityEnabled = useFeatureFlag('advisor.edge_parity');
   const [hasConventionalSystems, setHasConventionalSystems] = useState(true);
   const [hasEdgeDevices, setHasEdgeDevices] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
   const {
     data: edge,
     isSuccess: edgeQuerySuccess,
     isError: edgeError,
     error: edgeErrorMessage,
+    isLoading: isEdgeLoading,
   } = useGetEdgeDevicesQuery();
 
   const {
@@ -113,13 +112,26 @@ export const AdvisorRoutes = () => {
     isSuccess: conventionalQuerySuccess,
     isError: conventionalError,
     error: conventErrorMessage,
+    isLoading: isConventionalLoading,
   } = useGetConventionalDevicesQuery();
 
   useEffect(() => {
-    setHasEdgeDevices(edge?.total > 0);
-    setHasConventionalSystems(conventional?.total > 0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [edgeQuerySuccess, conventionalQuerySuccess]);
+    if (edgeQuerySuccess) {
+      setHasEdgeDevices(edge?.total > 0);
+    }
+    if (conventionalQuerySuccess) {
+      setHasConventionalSystems(conventional?.total > 0);
+    }
+
+    if (!isEdgeLoading && !isConventionalLoading) {
+      setIsLoading(false);
+    }
+  }, [
+    edgeQuerySuccess,
+    conventionalQuerySuccess,
+    isEdgeLoading,
+    isConventionalLoading,
+  ]);
 
   useEffect(() => {
     if (edgeErrorMessage?.status || conventErrorMessage?.status) {
@@ -141,9 +153,13 @@ export const AdvisorRoutes = () => {
     ? hasEdgeDevices || hasConventionalSystems
     : hasConventionalSystems;
 
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <AsyncComponent
-      appId={'advisor_zero_state'}
+      appId="advisor_zero_state"
       appName="dashboard"
       module="./AppZeroState"
       scope="dashboard"
@@ -156,10 +172,14 @@ export const AdvisorRoutes = () => {
           value={{ hasConventionalSystems, hasEdgeDevices, edgeQuerySuccess }}
         >
           <Routes>
-            {Object.entries(routes).map(
-              ([key, { path, component: Component }]) => (
-                <Route key={key} path={path} element={<Component />} />
-              )
+            {routeConfig.map(({ key, paths, component: Component, props }) =>
+              paths.map((path) => (
+                <Route
+                  key={`${key}-${path}`}
+                  path={path}
+                  element={<Component {...props} />}
+                />
+              ))
             )}
           </Routes>
         </AccountStatContext.Provider>
