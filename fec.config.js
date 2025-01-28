6aff849d315c36a3e140151c864b36c8933a60ca
Adonis Puente <adonis.puente@gmail.com>
2025-01-28 11:54:44 -0600

    fix(sentry): Update Sentry Flow

--- fec.config.js
+++ fec.config.js
@@ -8,33 +8,22 @@ module.exports = {
   proxyVerbose: false,
   devtool: 'hidden-source-map',
   plugins: [
-    ...(process.env.SENTRY_AUTH_TOKEN
-      ? [
-          sentryWebpackPlugin({
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-            org: 'red-hat-it',
-            project: 'advisor-rhel',
-            moduleMetadata: ({ release }) => ({
-              dsn: 'https://f8eb44de949e487e853185c09340f3cf@o490301.ingest.us.sentry.io/4505397435367424',
-              release,
-              org: 'red-hat-it',
-              project: 'advisor-rhel',
-            }),
-          }),
-        ]
-      : [
-          // Justs injects the debug ids
-          sentryWebpackPlugin({
-            org: 'red-hat-it',
-            project: 'advisor-rhel',
-            moduleMetadata: ({ release }) => ({
-              dsn: 'https://f8eb44de949e487e853185c09340f3cf@o490301.ingest.us.sentry.io/4505397435367424',
-              release,
-              org: 'red-hat-it',
-              project: 'advisor-rhel',
-            }),
-          }),
-        ]),
+    //Sentry Plugin should be at the end
+    process.env.ENABLE_SENTRY && [
+      sentryWebpackPlugin({
+        ...(process.env.SENTRY_AUTH_TOKEN && {
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+        }),
+        org: 'red-hat-it',
+        project: 'advisor-rhel',
+        moduleMetadata: ({ release }) => ({
+          dsn: `https://f8eb44de949e487e853185c09340f3cf@o490301.ingest.us.sentry.io/4505397435367424`,
+          org: 'red-hat-it',
+          project: 'advisor-rhel',
+          release,
+        }),
+      }),
+    ],
   ],
   moduleFederation: {
     shared: [
