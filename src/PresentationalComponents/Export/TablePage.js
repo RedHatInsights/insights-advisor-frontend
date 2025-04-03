e87810372d990622bd99b2afec146d6b7eb28b68
Michael Johnson <micjohns@micjohns-thinkpadp16vgen1.rmtusnc.csb>
2025-04-03 10:31:30 -0400

    feat(RHINENG-17046): use new pdf-generator for systems table
    This PR moves the systems table pdf export to the pdf-generator service
    and updates the pdf template.
    
    To run this locally, there is a multi-step process:
    
    In the config/webpack.config.js file, add the following under the routes object:
        '/apps/advisor/': {
           host: 'http://localhost:8003',
         },
         '/api/crc-pdf-generator/': {
           host: 'http://localhost:8000',
         },
    
    Then in a terminal window:
    [insights-chrome]$ npm run dev
    
    In a second terminal window, pull down pdf-generator make sure you're using node version 20. and run:
    [pdf-generator]$ PROXY_AGENT=http://squid.corp.redhat.com:3128 ASSETS_HOST=https://localhost:1337 API_HOST=https://console.stage.redhat.com npm run start:server
    
    In a third terminal window, navigate to pdf-generator and make sure you're using node version 20. Run:
    [pdf-generator]$ podman-compose up
    
    Finally, in a fourth terminal window, navigate to insights-advisor-frontend and run:
    [insights-advisor-frontend]$ npm run static
    
    Navigate to Advisor -> Systems
    Click the Download icon in the table toolbar
    Click Export to PDF
    
    This should render a PDF that looks similar to the previous PDF in layout, but different in design. The data should be the same as the previous PDF.
    
    This feature should be hidden behind a feature flag.
    
    You should be able to filter the data in the systems table and export
    the filtered data to pdf, but it appears that filtering doesn't work for
    pdf export. I've created a card to fix this issue: https://issues.redhat.com/browse/RHINENG-17627

---
+++ src/PresentationalComponents/Export/TablePage.js
@@ -0,0 +1,122 @@
+import {
+  BASE_URI,
+  LAST_SEEN,
+  NAME,
+  RECOMMENDATIONS,
+  TOTAL_RISK_CONSTANTS,
+} from '../../AppConstants';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Table, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import global_BackgroundColor_200 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_200';
+
+export const TablePage = ({ systems, styles }) => {
+  const header = [
+    { value: NAME, style: styles.nameColumn },
+    {
+      value: RECOMMENDATIONS,
+      style: { fontSize: 8, textAlign: 'center', width: '10px' },
+    },
+    {
+      value: TOTAL_RISK_CONSTANTS.CRITICAL,
+      style: { fontSize: 8, textAlign: 'center', width: '10px' },
+    },
+    {
+      value: TOTAL_RISK_CONSTANTS.IMPORTANT,
+      style: { fontSize: 8, textAlign: 'center', width: '10px' },
+    },
+    {
+      value: TOTAL_RISK_CONSTANTS.MODERATE,
+      style: { fontSize: 8, textAlign: 'center', width: '10px' },
+    },
+    {
+      value: TOTAL_RISK_CONSTANTS.LOW,
+      style: { fontSize: 8, textAlign: 'center', width: '10px' },
+    },
+    {
+      value: LAST_SEEN,
+      style: { fontSize: 8 },
+    },
+  ];
+  const hitColumns = [
+    'hits',
+    'critical_hits',
+    'important_hits',
+    'moderate_hits',
+    'low_hits',
+  ];
+
+  const headerBuilder = ({ value, style }) => (
+    <Th style={{ ...style, ...styles.header, ...styles.bold }}>{value}</Th>
+  );
+  const rowBuilder = ({ value }) => (
+    <Td noPadding={true} style={styles.row}>
+      {value}
+    </Td>
+  );
+  const rows = [
+    ...systems.map((system, idx) => {
+      const [, date, month, year, time] = new Date(system.last_seen)
+        .toUTCString()
+        .split(' ');
+      const sysDate = `${date} ${month} ${year}, ${time
+        .split(':')
+        .slice(0, 2)
+        .join(':')} UTC`;
+
+      const isOddRow = (idx + 1) % 2;
+      const customStyle = {
+        backgroundColor: global_BackgroundColor_200.var,
+      };
+      return (
+        <Tr
+          noPadding={true}
+          key={`${system.display_name}`}
+          style={isOddRow ? customStyle : {}}
+        >
+          <Td
+            key={system.system_uuid}
+            style={{ ...styles.nameColumn }}
+            noPadding={true}
+          >
+            <a
+              style={styles.link}
+              href={`${BASE_URI}/insights/advisor/systems/${system.system_uuid}/`}
+            >
+              {system.display_name}
+            </a>
+          </Td>
+          {...hitColumns.map((item) =>
+            rowBuilder({
+              value: system[item],
+            })
+          )}
+          <Td
+            key={system.last_seen}
+            style={{ ...styles.row, ...styles.lastSeenRow }}
+            noPadding={true}
+          >{`${sysDate}`}</Td>
+        </Tr>
+      );
+    }),
+  ];
+
+  return (
+    <Table variant="compact">
+      <Thead>
+        {header.map((colHeader) =>
+          headerBuilder({ value: colHeader.value, style: colHeader.style })
+        )}
+      </Thead>
+      <Tbody>{rows.map((row) => row)}</Tbody>
+    </Table>
+  );
+};
+
+TablePage.propTypes = {
+  systems: PropTypes.object,
+  page: PropTypes.number,
+  styles: PropTypes.node,
+};
+
+export default TablePage;
