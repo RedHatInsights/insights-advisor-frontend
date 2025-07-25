f40e4788e241d896e3c614e419b310ed9b0384fc
Mark Huth <mhuth@redhat.com>
2025-07-25 17:02:23 +1000

    fix(RHINENG-19383): Host Insights tab populates in IoP environment

--- src/Modules/SystemDetailWrapped.js
+++ src/Modules/SystemDetailWrapped.js
@@ -3,18 +3,26 @@ import { EnvironmentContext } from '../App';
 import { IOP_ENVIRONMENT_CONTEXT } from '../AppConstants';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
-import SystemDetail from './SystemDetail';
 import messages from '../../locales/translations.json';
 import { initStore } from '../Store';
+import SystemAdvisorWrapped from '../SmartComponents/SystemAdvisor/SystemAdvisorWrapped';
+import PropTypes from 'prop-types';
 
-export const SystemDetailWrapped = (props) => (
+const SystemDetailWrapped = (props) => (
   <IntlProvider locale="en" messages={messages}>
     <EnvironmentContext.Provider value={IOP_ENVIRONMENT_CONTEXT}>
       <Provider store={initStore()}>
-        <SystemDetail {...props} />
+        <SystemAdvisorWrapped
+          {...props}
+          IopRemediationModal={props.IopRemediationModal}
+        />
       </Provider>
     </EnvironmentContext.Provider>
   </IntlProvider>
 );
+
+SystemDetailWrapped.propTypes = {
+  IopRemediationModal: PropTypes.elementType,
+};
 
 export default SystemDetailWrapped;
