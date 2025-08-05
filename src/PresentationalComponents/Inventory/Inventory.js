c2388370a5c7cd4ff83d8c42a44f58a257f96ed6
Mark Huth <mhuth@redhat.com>
2025-08-05 20:38:37 +1000

    fix(RHINENG-19870): Disable Remediate button for manual recommendations

--- src/PresentationalComponents/Inventory/Inventory.js
+++ src/PresentationalComponents/Inventory/Inventory.js
@@ -419,6 +419,7 @@ const Inventory = ({
           <IopRemediationModal.WrappedComponent
             selectedIds={selectedIds}
             iopData={resolutions}
+            isDisabled={isRemediationButtonDisabled}
           />
         ) : (
           <RemediationButton
