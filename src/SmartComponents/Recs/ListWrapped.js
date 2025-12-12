3ec68230b98c04be8123f1aec6311466f5dec808
Mark Huth <mhuth@redhat.com>
2025-12-13 05:22:30 +1000

    feat(RHINENG-22380): Check IOP user has permission to disable rules

--- src/SmartComponents/Recs/ListWrapped.js
+++ src/SmartComponents/Recs/ListWrapped.js
@@ -6,17 +6,37 @@ import { IOP_ENVIRONMENT_CONTEXT } from '../../AppConstants';
 import { EnvironmentContext } from '../../App';
 import { initStore } from '../../Store';
 import ListIop from './ListIop';
+import PropTypes from 'prop-types';
 
 const dbStore = initStore();
 
-const ListWrapped = (props) => (
-  <IntlProvider locale="en" messages={messages}>
-    <EnvironmentContext.Provider value={IOP_ENVIRONMENT_CONTEXT}>
-      <Provider store={dbStore}>
-        <ListIop {...props} />
-      </Provider>
-    </EnvironmentContext.Provider>
-  </IntlProvider>
-);
+const ListWrapped = (props) => {
+  const { permissions } = props.getUserPermissions
+    ? props.getUserPermissions('advisor').then((res) => res)
+    : { permissions: [] };
+  const hasDisableRecPermission =
+    permissions.includes('advisor:disable-recommendations:write') ||
+    IOP_ENVIRONMENT_CONTEXT.isDisableRecEnabled;
+
+  return (
+    <IntlProvider locale="en" messages={messages}>
+      <EnvironmentContext.Provider
+        value={{
+          ...IOP_ENVIRONMENT_CONTEXT,
+          isDisableRecEnabled: hasDisableRecPermission,
+          CONTEXT_UPDATE_COMPONENT: 'ListWrapped', // just for testing
+        }}
+      >
+        <Provider store={dbStore}>
+          <ListIop {...props} />
+        </Provider>
+      </EnvironmentContext.Provider>
+    </IntlProvider>
+  );
+};
+
+ListWrapped.propTypes = {
+  getUserPermissions: PropTypes.func,
+};
 
 export default ListWrapped;
--- src/SmartComponents/Recs/RecommendationDetailsWrapped.js
+++ src/SmartComponents/Recs/RecommendationDetailsWrapped.js
@@ -8,6 +8,7 @@ import { initStore } from '../../Store';
 
 import axios from 'axios';
 import IopRecommendationDetails from './IopRecommendationDetails';
+import PropTypes from 'prop-types';
 
 window.insights = {
   chrome: {
@@ -34,14 +35,35 @@ export function responseDataInterceptor(response) {
 const instance = axios.create({ baseURL: '/insights_cloud/' });
 instance.interceptors.response.use(responseDataInterceptor);
 
-const RecommendationDetailsWrapped = (props) => (
-  <IntlProvider locale="en" messages={messages}>
-    <EnvironmentContext.Provider value={IOP_ENVIRONMENT_CONTEXT}>
-      <Provider store={initStore()}>
-        <IopRecommendationDetails {...props} axios={instance} />
-      </Provider>
-    </EnvironmentContext.Provider>
-  </IntlProvider>
-);
+const dbStore = initStore();
+
+const RecommendationDetailsWrapped = (props) => {
+  const { permissions } = props.getUserPermissions
+    ? props.getUserPermissions('advisor').then((res) => res)
+    : { permissions: [] };
+  const hasDisableRecPermission =
+    permissions.includes('advisor:disable-recommendations:write') ||
+    IOP_ENVIRONMENT_CONTEXT.isDisableRecEnabled;
+
+  return (
+    <IntlProvider locale="en" messages={messages}>
+      <EnvironmentContext.Provider
+        value={{
+          ...IOP_ENVIRONMENT_CONTEXT,
+          isDisableRecEnabled: hasDisableRecPermission,
+          CONTEXT_UPDATE_COMPONENT: 'RecommendationDetailsWrapped', // just for testing
+        }}
+      >
+        <Provider store={dbStore}>
+          <IopRecommendationDetails {...props} axios={instance} />
+        </Provider>
+      </EnvironmentContext.Provider>
+    </IntlProvider>
+  );
+};
+
+RecommendationDetailsWrapped.propTypes = {
+  getUserPermissions: PropTypes.func,
+};
 
 export default RecommendationDetailsWrapped;
