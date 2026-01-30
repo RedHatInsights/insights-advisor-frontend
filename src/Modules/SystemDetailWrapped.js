df7170f993480c51264c40e851064b6cca0f5d14
Fewwy <avoznese@redhat.com>
2026-01-30 14:15:42 +0100

    feat(RHINENG-23663): update iop permission logic and add tests

--- src/Modules/SystemDetailWrapped.js
+++ src/Modules/SystemDetailWrapped.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import { EnvironmentContext } from '../App';
-import { IOP_ENVIRONMENT_CONTEXT, PERMISSIONS } from '../AppConstants';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
 import messages from '../../locales/translations.json';
 import { initStore } from '../Store';
 import SystemAdvisorWrapped from '../SmartComponents/SystemAdvisor/SystemAdvisorWrapped';
 import PropTypes from 'prop-types';
-import { useRbac } from '../Utilities/Hooks';
+import { useIopEnvironmentContext } from '../Utilities/Hooks';
 
 const SystemDetailWrapped = (props) => {
-  const [[hasViewRecPermission]] = useRbac([PERMISSIONS.viewRecs]);
-  const viewRecPermission =
-    Array.isArray(hasViewRecPermission) && hasViewRecPermission.length === 0
-      ? false
-      : hasViewRecPermission;
+  const envContext = useIopEnvironmentContext();
 
   return (
     <IntlProvider locale="en" messages={messages}>
-      <EnvironmentContext.Provider
-        value={{
-          ...IOP_ENVIRONMENT_CONTEXT,
-          isAllowedToViewRec: viewRecPermission,
-        }}
-      >
+      <EnvironmentContext.Provider value={envContext}>
         <Provider store={initStore()}>
           <SystemAdvisorWrapped
             {...props}
--- src/SmartComponents/Recs/ListWrapped.js
+++ src/SmartComponents/Recs/ListWrapped.js
@@ -2,40 +2,19 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
 import messages from '../../../locales/translations.json';
-import { IOP_ENVIRONMENT_CONTEXT } from '../../AppConstants';
 import { EnvironmentContext } from '../../App';
 import { initStore } from '../../Store';
 import ListIop from './ListIop';
-import { useRbac } from '../../Utilities/Hooks';
-import { PERMISSIONS } from '../../AppConstants';
+import { useIopEnvironmentContext } from '../../Utilities/Hooks';
 
 const dbStore = initStore();
 
 const ListWrapped = (props) => {
-  const [[hasDisableRecPermission, hasViewRecPermission]] = useRbac([
-    PERMISSIONS.disableRec,
-    PERMISSIONS.viewRecs,
-  ]);
-
-  const disableRecPermission =
-    Array.isArray(hasDisableRecPermission) &&
-    hasDisableRecPermission.length === 0
-      ? false
-      : hasDisableRecPermission;
-  const viewRecPermission =
-    Array.isArray(hasViewRecPermission) && hasViewRecPermission.length === 0
-      ? false
-      : hasViewRecPermission;
+  const envContext = useIopEnvironmentContext();
 
   return (
     <IntlProvider locale="en" messages={messages}>
-      <EnvironmentContext.Provider
-        value={{
-          ...IOP_ENVIRONMENT_CONTEXT,
-          isDisableRecEnabled: disableRecPermission,
-          isAllowedToViewRec: viewRecPermission,
-        }}
-      >
+      <EnvironmentContext.Provider value={envContext}>
         <Provider store={dbStore}>
           <ListIop {...props} />
         </Provider>
