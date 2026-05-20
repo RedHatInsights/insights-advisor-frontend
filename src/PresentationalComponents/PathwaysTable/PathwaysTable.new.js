e8d19b3be462affe17ef2ffa435a6a8aa3b26330
Fewwy <avoznese@redhat.com>
2026-05-20 21:49:49 +0200

    feat(RHINENG-25234): cleaned up manual handling of the table state

--- src/PresentationalComponents/PathwaysTable/PathwaysTable.new.js
+++ src/PresentationalComponents/PathwaysTable/PathwaysTable.new.js
@@ -1,7 +1,6 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { TableToolsTable, TableStateProvider } from 'bastilian-tabletools';
-import { SkeletonTable } from '@patternfly/react-component-groups';
 import { useSelector } from 'react-redux';
 import { ErrorState } from '@redhat-cloud-services/frontend-components/ErrorState';
 import { Table } from '@patternfly/react-table';
@@ -13,7 +12,6 @@ import useAdvisorTableDefaults from '../../Utilities/useAdvisorTableDefaults';
 
 const PathwaysTableInner = ({ isTabActive, selectedTags, workloads }) => {
   const advisorTableDefaults = useAdvisorTableDefaults();
-  const hasLoadedOnce = useRef(false);
   const filterConfig = useMemo(() => ({ filterConfig: filters }), []);
 
   const additionalParams = useMemo(() => {
@@ -36,12 +34,6 @@ const PathwaysTableInner = ({ isTabActive, selectedTags, workloads }) => {
     additionalParams,
   });
 
-  // Track if we've loaded data at least once
-  if (items) {
-    hasLoadedOnce.current = true;
-  }
-
-  // Combine default options with sortBy
   const tableOptions = useMemo(
     () => ({
       ...advisorTableDefaults,
@@ -55,13 +47,6 @@ const PathwaysTableInner = ({ isTabActive, selectedTags, workloads }) => {
       <Table>
         <ErrorState />
       </Table>
-    );
-  }
-
-  // Only show skeleton on INITIAL load, not during refetches
-  if (loading && !hasLoadedOnce.current) {
-    return (
-      <SkeletonTable columns={columns.map((c) => c.title)} variant="compact" />
     );
   }
 
