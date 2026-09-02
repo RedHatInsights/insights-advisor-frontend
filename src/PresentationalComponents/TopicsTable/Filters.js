7a76808d69eeb5e3865900443c7d4c5771a526e0
Aleksandr Voznesenskii <62722417+Fewwy@users.noreply.github.com>
2026-09-02 15:23:22 +0200

    fix(RHINENG-26564): fix topics table name filter and add tests (#2155)

--- src/PresentationalComponents/TopicsTable/Filters.js
+++ src/PresentationalComponents/TopicsTable/Filters.js
@@ -1,7 +1,14 @@
+const escapeRegExpForJsonQuery = (value) => {
+  const text = Array.isArray(value) ? value[0] : value;
+  return text.replace(/[.*+?^${}()|[\]\\]/g, '\\\\$&');
+};
+
 export const nameFilter = {
   type: 'text',
   label: 'Name',
   filterAttribute: 'name',
+  filterSerialiser: (filterConfigItem, value) =>
+    `regex(.${filterConfigItem.filterAttribute}, "${escapeRegExpForJsonQuery(value)}", "i")`,
 };
 
 export default [nameFilter];
